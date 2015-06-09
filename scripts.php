b4c7d37f96409f067a49bcc7fad2046a8ceb4cd8
Andréas <pinbouen.andreas@gmail.com>
2015-06-09 17:22:34 +0200

    Added custom popover plugin.

--- scripts.php
+++ scripts.php
@@ -9,5 +9,6 @@
 <script src="assets/js/swap-menus.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="assets/js/ekko-lightbox.min.js"></script>
+<script src="assets/js/bootstrap-popover-x.min.js"></script>
 <script src="assets/js/js.cookie.js"></script>
 <script src="assets/js/Chart.js"></script>
