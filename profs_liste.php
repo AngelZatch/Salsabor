888aa5f91e29b34957431a798f2460a2c5e7adbd
Andréas <pinbouen.andreas@gmail.com>
2015-06-11 12:27:20 +0200

    Starting work for displaying prices, teachers and binding data to each other.

---
+++ profs_liste.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Template - Salsabor</title>
+    <?php include "includes.php";?>
+</head>
+<body>
+  <?php include "nav.php";?>
+   <div class="container-fluid">
+       <div class="row">
+           <?php include "side-menu.php";?>
+           <div class="col-sm-10 main">
+               <h1 class="page-title"><span class="glyphicon glyphicon-blackboard"></span> Base Professeurs</h1>
+               <div class="btn-toolbar">
+                   <a href="" role="button" class="btn btn-primary disabled"><span class="glyphicon glyphicon-plus"></span> Ajouter un professeur</a>
+               </div>
+           </div>
+       </div>
+   </div>
+   <?php include "scripts.php";?>    
+</body>
+</html>
--- side-menu.php
+++ side-menu.php
@@ -3,9 +3,11 @@
       <a id="close" onClick="toggleSideMenu()"><span></span></a>
       <li><a href="dashboard.php" class="main-option"><span class="glyphicon glyphicon-dashboard"></span> Panneau principal</a></li>
       <li><a href="" class="main-option"><span class="glyphicon glyphicon-user"></span> Base Client</a></li>
-      <li><a href="" class="main-option"><span class="glyphicon glyphicon-cd"></span> Forfaits</a></li>
+      <li><a href="" class="main-option"><span class="glyphicon glyphicon-credit-card"></span> Forfaits</a></li>
+      <li><a href="tarifs.php" class="main-option <?php if(basename($_SERVER['PHP_SELF'])=='tarifs.php') echo "active";?>"><span class="glyphicon glyphicon-scale"></span> Tarifs</a></li>
       <li><a href="cours_liste.php" class="main-option <?php if(basename($_SERVER['PHP_SELF'])=='cours_liste.php') echo "active";?>"><span class="glyphicon glyphicon-time"></span> Planning et Réservations</a></li>
       <li><a href="staff_liste.php?rank=0" class="main-option <?php if(basename($_SERVER['PHP_SELF'])=='staff_liste.php') echo "active";?>"><span class="glyphicon glyphicon-briefcase"></span> Gestion du staff</a></li>
+      <li><a href="profs_liste.php" class="main-option <?if(basename($_SERVER['PHP_SELF']) == 'profs_liste.php') echo "active";?>"><span class="glyphicon glyphicon-blackboard"></span> Base Professeurs</a></li>
       <li><a href="" class="main-option"><span class="glyphicon glyphicon-hdd"></span> Ressources</a></li>
       <li><a href="" class="main-option"><span class="glyphicon glyphicon-calendar"></span> Evènements</a></li>
       <li><a href="" class="main-option"><span class="glyphicon glyphicon-thumbs-up"></span> Réseaux Sociaux</a></li>
@@ -21,9 +23,11 @@
         <a id="open" onClick="toggleSideMenu()"><span></span></a>
         <li><a href="dashboard.php" class="main-option" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Panneau d'administration"><span class="glyphicon glyphicon-dashboard"></span></a></li>
         <li><a href="dashboard.php" class="main-option" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Base client"><span class="glyphicon glyphicon-user"></span></a></li>
-        <li><a href="dashboard.php" class="main-option" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Forfaits"><span class="glyphicon glyphicon-cd"></span></a></li>
+        <li><a href="dashboard.php" class="main-option" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Forfaits"><span class="glyphicon glyphicon-credit-card"></span></a></li>
+        <li><a href="tarifs.php" class="main-option <?php if(basename($_SERVER['PHP_SELF'])=='tarifs.php') echo "active";?>" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Tarifs"><span class="glyphicon glyphicon-scale"></span></a></li>
         <li><a href="cours_liste.php" class="main-option <?php if(basename($_SERVER['PHP_SELF'])=='cours_liste.php') echo "active";?>" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Planning et Réservations"><span class="glyphicon glyphicon-time"></span></a></li>
         <li><a href="staff_liste.php?rank=0" class="main-option <?php if(basename($_SERVER['PHP_SELF'])=='staff_liste.php') echo "active";?>" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Gestion du staff"><span class="glyphicon glyphicon-briefcase"></span></a></li>
+        <li><a href="profs_liste.php?rank=0" class="main-option <?php if(basename($_SERVER['PHP_SELF'])=='profs_liste.php') echo "active";?>" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Base Professeurs"><span class="glyphicon glyphicon-blackboard"></span></a></li>
         <li><a href="dashboard.php" class="main-option" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Ressources"><span class="glyphicon glyphicon-hdd"></span></a></li>
         <li><a href="dashboard.php" class="main-option" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Evènements"><span class="glyphicon glyphicon-calendar"></span></a></li>
         <li><a href="dashboard.php" class="main-option" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Réseaux Sociaux"><span class="glyphicon glyphicon-thumbs-up"></span></a></li>
