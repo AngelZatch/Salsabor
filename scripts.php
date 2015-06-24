f1d48e291794f6cd47a7e1e0fec29dd5461fd890
Andréas <pinbouen.andreas@gmail.com>
2015-06-24 16:25:24 +0200

    Added bootstrap-toggle to plugins

--- scripts.php
+++ scripts.php
@@ -10,6 +10,7 @@
 <script src="assets/js/jquery-dateFormat.min.js"></script>
 <script src="assets/js/ekko-lightbox.min.js"></script>
 <script src="assets/js/bootstrap-popover-x.min.js"></script>
+<script src="assets/js/bootstrap-toggle.min.js"></script>
 <script src="assets/js/js.cookie.js"></script>
 <script src="assets/js/Chart.js"></script>
 
