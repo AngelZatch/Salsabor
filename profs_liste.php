a3aeab2a972db6c1d580572590d880acb10602c6
Andréas <pinbouen.andreas@gmail.com>
2015-07-01 17:19:58 +0200

    Displaying all teachers.

--- profs_liste.php
+++ profs_liste.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'functions/db_connect.php';
+$db = PDOFactory::getConnection();
+?>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -13,7 +17,36 @@
                <h1 class="page-title"><span class="glyphicon glyphicon-blackboard"></span> Base Professeurs</h1>
                <div class="btn-toolbar">
                    <a href="" role="button" class="btn btn-primary disabled"><span class="glyphicon glyphicon-plus"></span> Ajouter un professeur</a>
-               </div>
+               </div> <!-- btn-toolbar -->
+               <?php
+                $profs = $db->query('SELECT * FROM professeurs');
+                ?>
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th class="col-sm-4">Professeur</th>
+                                <th class="col-sm-5"></th>
+                                <th class="col-sm-3"></th>
+                            </tr>
+                        </thead>
+                        <tbody id="filter-enabled">
+                            <?php
+                            while($row_profs = $profs->fetch(PDO::FETCH_ASSOC)){
+                            ?>
+                            <tr>
+                                <td class="col-sm-4">
+                                <?php
+                                    echo $row_profs['prenom'].' '.$row_profs['nom'];
+                                ?>
+                                </td>
+                            </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
            </div>
        </div>
    </div>
