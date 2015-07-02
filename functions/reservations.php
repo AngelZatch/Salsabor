9c9519dd0228b6faa4ca0b68bb6ce38c3563cc9a
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2015-07-02 13:24:58 +0200

    added FPDI library

--- functions/reservations.php
+++ functions/reservations.php
@@ -1,6 +1,36 @@
 <?php
 require_once "db_connect.php";
-include "librairies/fpdf.php";
+include "librairies/fpdf/fpdf.php";
+include "librairies/fpdi/fpdi.php";
+
+class ReservationPDF extends FPDF
+{
+	var $col = 0;
+	var $y0;
+	
+	function Header(){
+		global $titre;
+		$this->setFont('Arial', 'B', 15);
+		$w = $this->GetStringWidth($titre)+6;
+		$this->SetX((210 - $w)/2);
+		$this->SetLineWidth(1);
+		$this->Cell($w,9,$titre,1,1,'C',false);
+		$this->Ln(10);
+		$this->y0 = $this->GetY();
+	}
+	
+	function PersonalInformation(){
+		$db = PDOFactory::getConnection();
+		$eleve = $db->prepare('SELECT * FROM adherents WHERE eleve_id=?');
+		$eleve->bindValue(1, 1);
+		$eleve->execute();
+		$res_eleve = $eleve->fetch(PDO::FETCH_ASSOC);
+	}
+	
+	function ReservationDetails(){
+		
+	}
+}
 
 function addResa(){
 	$demandeur = $_POST['identite'];
@@ -15,7 +45,36 @@ function addResa(){
 	$priorite = $_POST['priorite'];
 	$paiement = $_POST['paiement'];
 	
-	$db = PDOFactory::getConnection();
+	$pdf = new FPDI();
+	$pdf->AddPage();
+	$pdf->SetSourceFile("librairies/Salsabor-resa-facture.pdf");
+	$tplIdx = $pdf->importPage(1);
+	$pdf->useTemplate($tplIdx, 0, 0, 210);
+	$pdf->setXY(10, 80);
+	$pdf->SetFont('Arial', '', 12);
+	
+	$infos = "M.\n".$demandeur."\n51, rue Servan - 75011 Paris\npinbouen.andreas@gmail.com\nTél : 06 82 71 11 71";
+	$infos = iconv('UTF-8', 'windows-1252', $infos);
+	$pdf->MultiCell(0, 7, $infos);
+	/**	
+	// Réservation
+	if($priorite == 0) {
+		$textPriorite = 'libre (Attention : une réservation libre peut être supprimée sans préavis au profit d\'un cours)';
+	} else $textPriorite = 'payée';
+	$reservation = "Détail de la réservation : \n".$_POST['prestation']."\n Le ".date_create($date_debut)->format('d/m/Y')." de ".date_create($date_debut)->format('H:i')." à ".date_create($date_fin)->format('H:i')."\nRéservation ".$textPriorite;
+	$reservation = iconv('UTF-8', 'windows-1252', $reservation);
+	
+	$pdf->SetFont('Arial', '', 15);
+	$pdf->MultiCell(0, 10, "Informations du Demandeur : \n".$infos, 1, 1);
+	$pdf->MultiCell(0, 10, $reservation, 1, 1);**/
+	$pdf->Output();
+	
+	/**$pdf = new ReservationPDF();
+	$titre = "FACTURE DE RESERVATION DE SALLE";
+	$pdf->setTitle($titre);
+	$pdf->Output();**/
+	
+	/**$db = PDOFactory::getConnection();
 	try{
 		$db->beginTransaction();
 		$insertResa = $db->prepare('INSERT INTO reservations(reservation_personne, type_prestation, reservation_start, reservation_end, reservation_salle, reservation_unite, reservation_prix, priorite, paiement_effectue)
@@ -33,15 +92,10 @@ function addResa(){
 		
 		$db->commit();
 		// On génère le PDF "facture" une fois que la transaction est terminée
-		/**$pdf = new FPDF();
-		$pdf->AddPage();
-		$pdf->SetFont('Arial', 'B', 15);
-		$pdf->Cell(40,10, $demandeur);
-		$pdf->Output();**/
 	} catch(PDOException $e){
 		$db->rollBack();
 		var_dump($e->getMessage());
-	}
+	}**/
 }
 
 function deleteResa(){
