377f01972ef64fb476fc3014ff049ce7fc00193e
Andréas <pinbouen.andreas@gmail.com>
2015-07-16 18:29:56 +0200

    can now edit bundles

--- forfait_details.php
+++ forfait_details.php
@@ -8,10 +8,10 @@ $data = $_GET["id"];
 $queryProduit = $db->prepare("SELECT * FROM produits WHERE produit_id=?");
 $queryProduit->bindParam(1, $data);
 $queryProduit->execute();
-$forfait = $queryProduit->fetch(PDO::FETCH_ASSOC);
+$produit = $queryProduit->fetch(PDO::FETCH_ASSOC);
 
 if(isset($_POST["edit"])){
-    /**if(isset($_POST["volume_horaire"])){
+    if(isset($_POST["volume_horaire"])){
         $tarif_horaire = $_POST["tarif_global"]/$_POST["volume_horaire"];
     } else {
         $tarif_horaire = 0;
@@ -26,32 +26,43 @@ if(isset($_POST["edit"])){
     
     try{
         $db->beginTransaction();
-        $new = $db->prepare("INSERT INTO produits(produit_nom, description, volume_horaire, validite_initiale, tarif_horaire, tarif_global, date_activation ,date_desactivation, actif, echeances_paiement, autorisation_report)
-        VALUES(:intitule, :description, :volume_horaire, :validite, :tarif_horaire, :tarif_global, :date_activation, :date_limite_achat, :actif, :echeances, :autorisation_report)");
-        $new->bindParam(':intitule', $_POST["intitule"]);
-        $new->bindParam(':description', $_POST["description"]);
-        $new->bindParam(':volume_horaire', $_POST["volume_horaire"]);
-        $new->bindParam(':validite', $validite);
-        $new->bindParam(':tarif_horaire', $tarif_horaire);
-        $new->bindParam(':tarif_global', $_POST["tarif_global"]);
-        $new->bindParam(':date_activation', $_POST["date_activation"]);
-        $new->bindParam(':date_limite_achat', $_POST["date_limite_achat"]);
-        $new->bindParam(':actif', $actif);
-        $new->bindParam(':echeances', $_POST["echeances"]);
-        $new->bindParam(':autorisation_report', $arep);
-        $new->execute();
+        $edit = $db->prepare("UPDATE produits SET produit_nom = :intitule,
+                                                description = :description,
+                                                volume_horaire = :volume_horaire,
+                                                validite_initiale = :validite,
+                                                tarif_horaire = :tarif_horaire,
+                                                tarif_global = :tarif_global,
+                                                date_activation = :date_activation,
+                                                date_desactivation = :date_limite_achat,
+                                                actif = :actif,
+                                                echeances_paiement = :echeances,
+                                                autorisation_report = :autorisation_report
+                                                WHERE produit_id = :id");
+        $edit->bindParam(':intitule', $_POST["intitule"]);
+        $edit->bindParam(':description', $_POST["description"]);
+        $edit->bindParam(':volume_horaire', $_POST["volume_horaire"]);
+        $edit->bindParam(':validite', $validite);
+        $edit->bindParam(':tarif_horaire', $tarif_horaire);
+        $edit->bindParam(':tarif_global', $_POST["tarif_global"]);
+        $edit->bindParam(':date_activation', $_POST["date_activation"]);
+        $edit->bindParam(':date_limite_achat', $_POST["date_limite_achat"]);
+        $edit->bindParam(':actif', $actif);
+        $edit->bindParam(':echeances', $_POST["echeances"]);
+        $edit->bindParam(':autorisation_report', $arep);
+        $edit->bindParam(':id', $data);
+        $edit->execute();
         $db->commit();
         header("Location: forfaits.php");
     }catch (PDOException $e){
         $db->rollBack();
         var_dump($e->getMessage());
-    }**/
+    }
 }
 ?>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Détails du forfait <?php echo $forfait["produit_nom"];?> | Salsabor</title>
+    <title>Détails du forfait <?php echo $produit["produit_nom"];?> | Salsabor</title>
     <?php include "includes.php";?>
 </head>
 <body>
@@ -60,44 +71,44 @@ if(isset($_POST["edit"])){
        <div class="row">
            <?php include "side-menu.php";?>
            <div class="col-sm-10 main">
-               <h1 class="page-title"><span class="glyphicon glyphicon-credit-card"></span> Forfait <?php echo $forfait["produit_nom"];?></h1>
+               <h1 class="page-title"><span class="glyphicon glyphicon-credit-card"></span> Forfait <?php echo $produit["produit_nom"];?></h1>
                 <div class="col-sm-9" id="solo-form">
-                    <form action="forfait_add.php" method="post">
+                    <form action="forfait_details.php?id=<?php echo $data;?>" method="post">
                       <div class="btn-toolbar">
                           <a href="forfaits.php" role="button" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span> Retour aux forfaits</a>
-                          <input type="submit" name="add" role="button" class="btn btn-primary" value="ENREGISTRER">
+                          <input type="submit" name="edit" role="button" class="btn btn-primary" value="ENREGISTRER">
                       </div><!-- btn-toolbar -->
                       <div class="form-group">
                          <label for="intitule">Intitulé</label>
-                          <input type="text" class="form-control" name="intitule" placeholder="Nom du produit">
+                          <input type="text" class="form-control" name="intitule" value="<?php echo $produit["produit_nom"];?>" placeholder="Nom du produit">
                       </div>
                       <div class="form-group">
                           <label for="description">Description</label>
-                          <textarea rows="5" class="form-control" name="description" placeholder="Facultatif. Tentez d'être succinct !"></textarea>
+                          <textarea rows="5" class="form-control" name="description" value="<?php echo $produit["description"];?>" placeholder="Facultatif. Tentez d'être succinct !"></textarea>
                       </div>
                       <div class="form-group">
                           <label for="volume_horaire">Volume de cours (en heures)</label>
-                          <input type="text" class="form-control" name="volume_horaire" placeholder="Exemple : 10">
+                          <input type="text" class="form-control" name="volume_horaire" value="<?php echo $produit["volume_horaire"];?>" placeholder="Exemple : 10">
                       </div>
                       <div class="form-group">
                           <label for="validite">Durée de validité (à partir de l'achat, en semaines)</label>
-                          <input type="text" class="form-control" name="validite" placeholder="Exemple : 48">
+                          <input type="text" class="form-control" name="validite" value="<?php echo $produit["validite_initiale"] / 7;?>" placeholder="Exemple : 48">
                       </div>
                       <div class="form-group">
                           <label for="tarif_global">Prix d'achat</label>
-                          <input type="text" class="form-control" name="tarif_global">
+                          <input type="text" class="form-control" name="tarif_global" value="<?php echo $produit["tarif_global"];?>">
                       </div>
                       <div class="form-group">
                           <label for="date_activation">Date de mise à disposition à l'achat (laissez vide pour une activation dès la validation)</label>
-                          <input type="date" class="form-control" name="date_activation">
+                          <input type="date" class="form-control" name="date_activation" value="<?php echo $produit["date_activation"];?>">
                       </div>
                       <div class="form-group">
                           <label for="date_limite_achat">Date limite d'achat possible (laissez vide pour une activation pendant une durée indéfinie)</label>
-                          <input type="date" class="form-control" name="date_limite_achat">
+                          <input type="date" class="form-control" name="date_limite_achat" value="<?php echo $produit["date_desactivation"];?>">
                       </div>
                       <div class="form-group">
                           <label for="echeances">Nombre d'échéances de paiement autorisées</label>
-                          <input type="text" class="form-control" name="echeances">
+                          <input type="text" class="form-control" name="echeances" value="<?php echo $produit["echeances_paiement"];?>">
                       </div>
                       <div class="form-group">
                           <label for="arep">Autoriser l'extension de validité ?</label>
