7cf8ca560e65610c506cb0dce5eaef45228fbc03
Andréas <pinbouen.andreas@gmail.com>
2015-07-16 18:41:57 +0200

    added buttons+page  to sell bundle

--- dashboard.php
+++ dashboard.php
@@ -15,6 +15,9 @@ $db = PDOFactory::getConnection();
            <?php include "side-menu.php";?>
            <div class="col-sm-9 main">
            <h1><span class="glyphicon glyphicon-dashboard"></span> Panneau principal</h1>
+           <a class="btn btn-primary btn-big" href="eleve_add.php">INSCRIRE UN NOUVEL ADHERENT</a>
+           <a class="btn btn-primary btn-big" href="vente_forfait.php">REALISER UNE VENTE DE FORFAIT</a>
+           <a class="btn btn-primary btn-big">LIRE UNE CARTE ADHERENT</a>
            </div>
        </div>
    </div>
---
+++ vente_forfait.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'functions/db_connect.php';
+$db = PDOFactory::getConnection();
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Vente | Salsabor</title>
+    <?php include "includes.php";?>
+</head>
+<body>
+  <?php include "nav.php";?>
+   <div class="container-fluid">
+       <div class="row">
+           <?php include "side-menu.php";?>
+           <div class="col-sm-10 main">
+               <h1 class="page-title"><span class="glyphicon glyphicon-pencil"></span> Vente d'un produit</h1>
+           </div>
+       </div>
+   </div>
+   <?php include "scripts.php";?>    
+</body>
+</html>
