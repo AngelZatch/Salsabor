75aa7533320a4f70b03bedb0730343b43a7f953b
Andréas <pinbouen.andreas@gmail.com>
2015-07-17 12:28:51 +0200

    more friendly welcome from dashboard

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,7 @@ $db = PDOFactory::getConnection();
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Template - Salsabor</title>
+    <title>Accueil d'administration | Salsabor</title>
     <?php include "includes.php";?>
 </head>
 <body>
@@ -14,7 +14,7 @@ $db = PDOFactory::getConnection();
        <div class="row">
            <?php include "side-menu.php";?>
            <div class="col-sm-9 main">
-           <h1><span class="glyphicon glyphicon-dashboard"></span> Panneau principal</h1>
+           <h1><span class="glyphicon glyphicon-dashboard"></span> Bonjour !</h1>
            <a class="btn btn-primary btn-big" href="eleve_add.php">INSCRIRE UN NOUVEL ADHERENT</a>
            <a class="btn btn-primary btn-big" href="vente_forfait.php">REALISER UNE VENTE DE FORFAIT</a>
            <a class="btn btn-primary btn-big">LIRE UNE CARTE ADHERENT</a>
