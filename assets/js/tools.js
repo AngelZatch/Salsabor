e3a337a591d4e0660f04a098b07b3e1fd2af4f7f
Andréas <pinbouen.andreas@gmail.com>
2015-07-17 12:26:33 +0200

    fixed display from minutes to months.

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,5 +1,5 @@
 // Insert la date d'aujourd'hui dans un input de type date supportant la fonctionnalité (#today_possible)
 function insertToday(){
-    var today = new moment().format("YYYY-mm-DD");
+    var today = new moment().format("YYYY-MM-DD");
     $("#today_possible").val(today);
 }
