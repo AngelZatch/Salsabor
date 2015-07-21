bd1e8b9bf7d1bba97eeb0a16b9f66f0c6803d5a8
Andréas <pinbouen.andreas@gmail.com>
2015-07-21 17:15:17 +0200

    auto-extend bundle duration with holidays

--- functions/ventes.php
+++ functions/ventes.php
@@ -14,6 +14,25 @@ function vente(){
     
     $actif = ($_POST["date_activation"]>$date_achat)?0:1;
     
+    $queryHoliday = $db->prepare("SELECT * FROM jours_chomes WHERE date_chomee >= ? AND date_chomee <= ?");
+    $queryHoliday->bindParam(1, $_POST["date_activation"]);
+    $queryHoliday->bindParam(2, $_POST["date_expiration"]);
+    $queryHoliday->execute();
+    
+    $j = 0;
+    
+    for($i = 1; $i <= $queryHoliday->rowCount(); $i++){
+        $exp_date = date("Y-m-d 00:00:00",strtotime($_POST["date_expiration"].'+'.$i.'DAYS'));
+        $checkHoliday = $db->prepare("SELECT * FROM jours_chomes WHERE date_chomee=?");
+        $checkHoliday->bindParam(1, $exp_date);
+        $checkHoliday->execute();
+        if($checkHoliday->rowCount() != 0){
+            $j++;
+        }
+        $totalOffset = $i + $j;
+        $new_exp_date = date("Y-m-d 00:00:00",strtotime($_POST["date_expiration"].'+'.$totalOffset.'DAYS'));
+    }
+    
     try{
         $db->beginTransaction();
         $new = $db->prepare("INSERT INTO produits_adherents(id_adherent, id_produit, date_achat, date_activation, date_expiration, volume_cours, prix_achat, actif, arep)
@@ -22,7 +41,7 @@ function vente(){
         $new->bindParam(':produit_id', $_POST["produit"]);
         $new->bindParam(':date_achat', $date_achat);
         $new->bindParam(':date_activation', $_POST["date_activation"]);
-        $new->bindParam(':date_expiration', $_POST["date_expiration"]);
+        $new->bindParam(':date_expiration', $new_exp_date);
         $new->bindParam(':volume_horaire', $_POST["volume_horaire"]);
         $new->bindParam(':prix_achat', $_POST["prix_achat"]);
         $new->bindParam(':actif', $actif);
