86c3b3ed922c735595a31349522230d339c45404
Andréas <pinbouen.andreas@gmail.com>
2015-07-23 17:47:27 +0200

    WIP reading tag and autofilling table

--- nav.php
+++ nav.php
@@ -19,7 +19,7 @@ $queryPassages = $db->query("SELECT * FROM passages JOIN adherents ON passage_el
            <ul class="nav navbar-nav navbar-right">
              <li><a href=""><span class="glyphicon glyphicon-user"></span> Dev_Version</a></li>
               <li class="notification-option">
-                      <a href="" class="notification-icon"><span class="glyphicon glyphicon-map-marker"></span><span class="badge"><?php echo $nombrePassages;?></span></a>
+                      <a href="passages.php" class="notification-icon"><span class="glyphicon glyphicon-map-marker"></span><span class="badge"><?php echo $nombrePassages;?></span></a>
               </li>
               <li class="notification-option">
                       <a href="" class="notification-icon"><span class="glyphicon glyphicon-folder-open"></span></a>
--- read_rfid.php
+++ read_rfid.php
@@ -2,29 +2,22 @@
 require_once 'functions/db_connect.php';
 $db = PDOFactory::getConnection();
 
-$data = explode('*', $_GET["carte"]);
-$tag_rfid = $data[0];
-$ip_rfid = $data[1];
+if(isset($_GET["carte"])){
+    $data = explode('*', $_GET["carte"]);
+    $tag_rfid = $data[0];
+    $ip_rfid = $data[1];
+}
 
-$date = date_create('now')->format('Y-m-d H:i:s');
-
-$queryLecteur = $db->prepare('SELECT * FROM lecteurs_rfid JOIN salle ON lecteur_lieu=salle.salle_id WHERE lecteur_ip=?');
-$queryLecteur->bindParam(1, $ip_rfid);
-$queryLecteur->execute();
-$lecteur = $queryLecteur->fetch(PDO::FETCH_ASSOC);
-
-$solveAdherents = $db->prepare('SELECT * FROM adherents WHERE numero_rfid=?');
-$solveAdherents->bindParam(1, $tag_rfid);
-$solveAdherents->execute();
-$adherent = $solveAdherents->fetch(PDO::FETCH_ASSOC);
-
-$solveForfaits = $db->prepare('SELECT * FROM produits_adherents JOIN produits ON id_produit=produits.produit_id WHERE id_adherent=?');
-$solveForfaits->bindParam(1, $adherent["eleve_id"]);
-$solveForfaits->execute();
-
-$getCours = $db->prepare('SELECT * FROM cours WHERE cours_start > ?');
-$getCours->bindParam(1, $date);
-$getCours->execute();
+if(isset($_POST["add"])){
+    $new = $db->prepare("INSERT INTO passages(passage_eleve, passage_salle, passage_date)
+    VALUE(:tag, :salle, :date)");
+    $new->bindParam(':tag', $_POST["tag"]);
+    $new->bindParam(':salle', $_POST["salle"]);
+    $new->bindParam(':date', date_create('now')->format('Y-m-d H:i:s'));
+    $new->execute();
+    
+    header('Location: passages.php');
+}
 ?>
 <html>
 <head>
@@ -39,22 +32,16 @@ $getCours->execute();
            <?php include "side-menu.php";?>
            <div class="col-sm-10 main">
                <h1 class="page-title"><span class="glyphicon glyphicon-qrcode"></span> RFID</h1>
-               <?php if($queryLecteur->rowCount() != '0'){?>
-               <p>Tag numéro <?php echo $tag_rfid;?></p>
-               <p>Ce tag appartient à <?php echo $adherent["eleve_prenom"]." ".$adherent["eleve_nom"];?></p>
-               <p>Passage enregistré à : <?php echo $date;?></p>
-               <p>Correspond à la salle : <?php echo $lecteur["salle_name"];?></p>
-               
-               <p>Cet adhérent a les forfaits suivants :</p>
-                   <?php while($forfaits = $solveForfaits->fetch(PDO::FETCH_ASSOC)){ ?>
-               <p><?php echo $forfaits["produit_nom"];?></p>
-                   <?php } ?>
-               <p>L'adhérent a validé son passe pour au moins un des cours suivants : </p>
-                   <?php while($listeCours = $getCours->fetch(PDO::FETCH_ASSOC)){ ?>
-               <p><?php echo $listeCours["cours_intitule"]." (".$listeCours["cours_start"]." - ".$listeCours["cours_end"].")";?></p>
-                   <?php } ?>
-               <?php } ?>
-           </div>
+               <p>Simulez un passage RFID</p>
+               <form action="" method="post">
+                  <label for="tag">Tag</label>
+                   <input type="text" name="tag" class="form-control">
+                   
+                   <label for="salle">Salle du lecteur</label>
+                   <input type="text" name="salle" class="form-control">
+                   
+                   <input type="submit" value="SIMULER UN PASSAGE" name="add" class="btn btn-primary confirm-add">
+               </form>
        </div>
    </div>
    <?php include "scripts.php";?>    
