dc79538eddf98c1f246fa509e4688c25de970965
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2015-07-24 12:39:17 +0200

    temporary href to read RFIDs

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,7 @@ $db = PDOFactory::getConnection();
            <h1><span class="glyphicon glyphicon-dashboard"></span> Bonjour !</h1>
            <a class="btn btn-primary btn-big" href="eleve_add.php">INSCRIRE UN NOUVEL ADHERENT</a>
            <a class="btn btn-primary btn-big" href="vente_forfait.php">REALISER UNE VENTE DE FORFAIT</a>
-           <a class="btn btn-primary btn-big">LIRE UNE CARTE ADHERENT</a>
+           <a class="btn btn-primary btn-big" href="read_rfid.php">LIRE UNE CARTE ADHERENT</a>
            </div>
        </div>
    </div>
