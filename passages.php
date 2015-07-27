d4d3d2d88abf21023580cd3d8794138b385431c7
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2015-07-27 18:32:31 +0200

    RFID reading for courses
    Now displays opened courses and indicate RFID tags by place they've been
    registered.

--- passages.php
+++ passages.php
@@ -1,6 +1,15 @@
 <?php
 require_once 'functions/db_connect.php';
 $db = PDOFactory::getConnection();
+
+$compare_start = date_create('now')->format('Y-m-d H:i:s');
+$compare_end = date("Y-m-d H:i:s", strtotime($compare_start.'+90MINUTES'));
+$queryNextCours = $db->prepare("SELECT * FROM cours JOIN salle ON cours_salle=salle.salle_id WHERE (cours_start>=? AND cours_start<=?) OR ouvert=1 ORDER BY cours_start ASC");
+$queryNextCours->bindParam(1, $compare_start);
+$queryNextCours->bindParam(2, $compare_end);
+$queryNextCours->execute();
+
+
 ?>
 <html>
 <head>
@@ -16,9 +25,25 @@ $db = PDOFactory::getConnection();
            <div class="col-sm-10 main">
                <h1 class="page-title"><span class="glyphicon glyphicon-map-marker"></span> Passages</h1>
                <p id="current-time"></p>
-               <p id="last-edit">Aucun cours à venir pour le moment.</p>
-               <?php while($passages = $queryPassages->fetch(PDO::FETCH_ASSOC)) { ?>
-               <p><?php echo $passages["eleve_prenom"]." ".$passages["eleve_nom"]." enregistré à ".$passages["passage_date"];?></p>
+               <p id="last-edit"><?php echo ($queryNextCours->rowCount()!=0)?"Il y a ".$queryNextCours->rowCount()." cours à venir":"Aucun cours n'est à venir";?></p>
+               <?php while($nextCours = $queryNextCours->fetch(PDO::FETCH_ASSOC)){ ?>
+               <div class="panel panel-default">
+               	<div class="panel-heading">
+               		<div class="panel-title"><?php echo $nextCours["cours_intitule"]." (".$nextCours["salle_name"]." - de ".date_create($nextCours["cours_start"])->format("H:i")." à ".date_create($nextCours["cours_end"])->format("H:i").")";?><button class="btn btn-default">Valider</button></div>
+               	</div>
+               	<ul class="list-group">
+               		<?php								
+					  $queryPassages = $db->prepare("SELECT * FROM passages JOIN lecteurs_rfid ON passage_salle=lecteurs_rfid.lecteur_ip WHERE status=0 AND lecteur_lieu=?");
+					  $queryPassages->bindParam(1, $nextCours["cours_salle"]);
+					  $queryPassages->execute();
+					  while($passages = $queryPassages->fetch(PDO::FETCH_ASSOC)){ ?>
+						<li class="list-group-item">
+							<?php echo $passages["passage_eleve"]." Enregsitré à : ".$passages["passage_date"];?>
+							<span class="list-item-option"><span class="glyphicon glyphicon-ok"></span></span>	
+						</li>
+					<?php } ?>
+               	</ul>
+			   </div>
 			   <?php } ?>
            </div>
        </div>
