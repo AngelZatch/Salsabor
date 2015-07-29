8092f3ac8c3f329ec75e5d589bf5aa6fd9bbe592
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2015-07-29 12:41:17 +0200

    Fixed "write" RFID

--- functions/fetch_rfid.php
+++ functions/fetch_rfid.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../functions/db_connect.php";
 $db = PDOFactory::getConnection();
-$tag = "192.168.0.4";
+$tag = "192.168.0.3";
 $status = "1";
 $rfid = $db->prepare('SELECT passage_eleve FROM passages WHERE passage_salle=? AND status=?');
 $rfid->bindParam(1, $tag);
