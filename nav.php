77f58f455e1b7d8a0e89ddfb018f1ef78a0926df
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2015-08-03 12:44:27 +0200

    Badge color & display

--- nav.php
+++ nav.php
@@ -2,8 +2,6 @@
 require_once 'functions/db_connect.php';
 $db = PDOFactory::getConnection();
 
-$coursNotif = $db->query("SELECT * FROM cours WHERE paiement_effectue=0")->rowCount();
-
 $locationsNotif = $db->query("SELECT * FROM reservations WHERE paiement_effectue=0 AND priorite=1")->rowCount();
 
 $queryPassages = $db->query("SELECT * FROM passages JOIN adherents ON passage_eleve=adherents.numero_rfid");
@@ -16,14 +14,7 @@ $queryPassages = $db->query("SELECT * FROM passages JOIN adherents ON passage_el
        <div id="navbar" class="navbar-collapse collapse">
            <ul class="nav navbar-nav navbar-right">
              <li><a href=""><span class="glyphicon glyphicon-user"></span> Dev_Version</a></li>
-              <li class="notification-option">
-                      <a href="passages.php" class="notification-icon"><span class="glyphicon glyphicon-map-marker"></span><span class="badge" id="badge-passages"></span></a>
-              </li>
-              <li class="notification-option">
-                      <a href="" class="notification-icon"><span class="glyphicon glyphicon-folder-open"></span></a>
-              </li>
-              <li class="notification-option">
-                      <a href="" class="notification-icon"><span class="glyphicon glyphicon-calendar"></span><span class="badge"><?php echo $coursNotif;?></span></a>
+              <li class="notification-option"><a href="passages.php" class="notification-icon"><span class="glyphicon glyphicon-map-marker"></span><span class="badge" id="badge-passages"></span></a>
               </li>
               <li class="notification-option">
                       <a href="" class="notification-icon"><span class="glyphicon glyphicon-scale"></span><span class="badge"><?php echo $locationsNotif;?></span></a>
