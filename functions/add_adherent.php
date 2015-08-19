e0c756b0b256264094a84657c69e61d206b527bd
Andréas <pinbouen.andreas@gmail.com>
2015-08-19 15:27:14 +0200

    More updates of new pages

--- functions/add_adherent.php
+++ functions/add_adherent.php
@@ -2,10 +2,13 @@
 require_once "db_connect.php";
 $db = PDOFactory::getConnection();
 
+$accesWeb = 1;
+$estEleve = 1;
+
 try{
 	$db->beginTransaction();
-	$new = $db->prepare('INSERT INTO adherents(user_prenom, user_nom, user_rfid, date_naissance, date_inscription, rue, code_postal, ville, mail, telephone)
-	VALUES(:prenom, :nom, :rfid, :date_naissance, :date_inscription, :rue, :code_postal, :ville, :mail, :telephone)');
+	$new = $db->prepare('INSERT INTO users(user_prenom, user_nom, user_rfid, date_naissance, date_inscription, rue, code_postal, ville, mail, telephone, acces_web, est_eleve)
+	VALUES(:prenom, :nom, :rfid, :date_naissance, :date_inscription, :rue, :code_postal, :ville, :mail, :telephone, :acces_web, :est_eleve)');
 	$new->bindParam(':prenom', $_POST['identite_prenom']);
 	$new->bindParam(':nom', $_POST['identite_nom']);
 	$new->bindParam(':rfid', $_POST["rfid"]);
@@ -16,6 +19,8 @@ try{
 	$new->bindParam(':ville', $_POST['ville']);
 	$new->bindParam(':mail', $_POST['mail']);
 	$new->bindParam(':telephone', $_POST['telephone']);
+    $new->bindParam(':acces_web', $accesWeb);
+    $new->bindParam(':est_eleve', $estEleve);
 	$new->execute();
 	if(isset($_POST["rfid"])){
 		$delete = $db->prepare("DELETE FROM passages WHERE passage_eleve=? AND status=1");
