57af18eaceffeb9db8c4f04d3097417a7dce41f6
Andréas <pinbouen.andreas@gmail.com>
2015-08-19 18:07:58 +0200

    Adapting user details page to new db

--- user_details.php
+++ user_details.php
@@ -9,24 +9,55 @@ $queryDetails->bindValue(1, $data);
 $queryDetails->execute();
 $details = $queryDetails->fetch(PDO::FETCH_ASSOC);
 
-// On obtient l'historique de ses cours
-$queryHistory = $db->prepare('SELECT * FROM cours_participants JOIN cours ON cours_id_foreign=cours.cours_id JOIN niveau ON cours.cours_niveau=niveau.niveau_id JOIN salle ON cours.cours_salle=salle.salle_id WHERE eleve_id_foreign=?');
-$queryHistory->bindValue(1, $data);
-$queryHistory->execute();
+// Si l'élève est un professeur
+if($details["est_professeur"] == 1){
+    // On obtient l'historique de ses cours
+    $queryHistoryDonnes = $db->prepare('SELECT * FROM cours JOIN niveau ON cours_niveau=niveau.niveau_id JOIN salle ON cours_salle=salle.salle_id WHERE prof_principal=? OR prof_remplacant=? ORDER BY cours_start ASC');
+    $queryHistoryDonnes->bindValue(1, $data);
+    $queryHistoryDonnes->bindValue(2, $data);
+    $queryHistoryDonnes->execute();
 
-$queryResa = $db->prepare('SELECT * FROM reservations JOIN users ON reservation_personne=users.user_id JOIN prestations ON type_prestation=prestations_id JOIN salle ON reservation_salle=salle.salle_id WHERE reservation_personne=?');
-$queryResa->bindValue(1, $data);
-$queryResa->execute();
+    // Tarifs
+    $queryTarifs = $db->prepare('SELECT * FROM tarifs_professeurs JOIN prestations ON type_prestation=prestations.prestations_id WHERE prof_id_foreign=?');
+    $queryTarifs->bindValue(1, $data);
+    $queryTarifs->execute();
 
-$queryForfaits = $db->prepare('SELECT *, produits_adherents.date_activation AS dateActivation, produits_adherents.actif AS produitActif FROM produits_adherents JOIN users ON id_adherent=users.user_id JOIN produits ON id_produit=produits.produit_id WHERE id_adherent=? ORDER BY produitActif DESC, date_achat ASC');
-$queryForfaits->bindValue(1, $data);
-$queryForfaits->execute();
+    // Prestations
+    $queryPrestations = $db->query('SELECT * FROM prestations WHERE est_cours=1');
 
-$queryForfaitsActifs = $db->prepare("SELECT * FROM produits_adherents JOIN produits ON id_produit=produits.produit_id WHERE id_adherent=? AND produits_adherents.actif=1");
-$queryForfaitsActifs->bindParam(1, $data);
-$queryForfaitsActifs->execute();
+    // Types de ratio multiplicatif
+    $ratio = $db->query("SHOW COLUMNS FROM tarifs_professeurs LIKE 'ratio_multiplicatif'");
+    
+    // Prix de tous les cours
+    $totalPrice = 0;
+    $totalPaid = 0;
+    $totalDue = 0;
+}
 
-$queryEcheances = $db->query("SELECT * FROM produits_echeances JOIN produits_adherents ON id_produit_adherent=produits_adherents.id_transaction WHERE echeance_effectuee=2 AND id_adherent=$data")->rowCount();
+if($details["est_eleve"] == 1){
+    // On obtient l'historique de ses cours
+    $queryHistoryRecus = $db->prepare('SELECT * FROM cours_participants JOIN cours ON cours_id_foreign=cours.cours_id JOIN niveau ON cours.cours_niveau=niveau.niveau_id JOIN salle ON cours.cours_salle=salle.salle_id WHERE eleve_id_foreign=?');
+    $queryHistoryRecus->bindValue(1, $data);
+    $queryHistoryRecus->execute();
+
+    // On obtient l'historique de ses réservations
+    $queryResa = $db->prepare('SELECT * FROM reservations JOIN users ON reservation_personne=users.user_id JOIN prestations ON type_prestation=prestations_id JOIN salle ON reservation_salle=salle.salle_id WHERE reservation_personne=?');
+    $queryResa->bindValue(1, $data);
+    $queryResa->execute();
+
+    // On obtient l'historique de ses forfaits
+    $queryForfaits = $db->prepare('SELECT *, produits_adherents.date_activation AS dateActivation, produits_adherents.actif AS produitActif FROM produits_adherents JOIN users ON id_adherent=users.user_id JOIN produits ON id_produit=produits.produit_id WHERE id_adherent=? ORDER BY produitActif DESC, date_achat ASC');
+    $queryForfaits->bindValue(1, $data);
+    $queryForfaits->execute();
+
+    // Ainsi que les forfaits actifs
+    $queryForfaitsActifs = $db->prepare("SELECT * FROM produits_adherents JOIN produits ON id_produit=produits.produit_id WHERE id_adherent=? AND produits_adherents.actif=1");
+    $queryForfaitsActifs->bindParam(1, $data);
+    $queryForfaitsActifs->execute();
+
+    // Et on cherche à savoir si des échéances sont en retard
+    $queryEcheances = $db->query("SELECT * FROM produits_echeances JOIN produits_adherents ON id_produit_adherent=produits_adherents.id_transaction WHERE echeance_effectuee=2 AND id_adherent=$data")->rowCount();   
+}
 
 // Edit des informations
 if(isset($_POST["edit"])){
@@ -106,14 +137,20 @@ if(isset($_POST["edit"])){
                    <a href="adherents.php" role="button" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span> Retour à la liste des adhérents</a>
                 </div> <!-- btn-toolbar -->
                <h1 class="page-title"><span class="glyphicon glyphicon-user"></span> <?php echo $details["user_prenom"]." ".$details["user_nom"];?></h1>
-               <?php if($queryEcheances != 0){ ?>
+               <?php if($details["est_eleve"] == 1 && $queryEcheances != 0){ ?>
                <div class="alert alert-danger"><strong>Attention !</strong> Cet adhérent a des échéances en retard.</div>
                <?php } ?>
 			  <ul class="nav nav-tabs">
                    <li role="presentation" id="infos-toggle" class="active"><a>Informations personnelles</a></li>
-                   <li role="presentation" id="history-toggle"><a>Historique des cours</a></li>
-                   <li role="presentation" id="resa-toggle"><a>Historique des réservations</a></li>
-                   <li role="presentation" id="forfaits-toggle"><a>Historique des forfaits</a></li>
+                   <?php if($details["est_eleve"] == 1){ ?>
+                   <li role="presentation" id="history-suivis-toggle"><a>Cours suivis</a></li>
+                   <li role="presentation" id="resa-toggle"><a>Réservations</a></li>
+                   <li role="presentation" id="forfaits-toggle"><a>Forfaits/Abonnements</a></li>
+                    <?php } if($details["est_professeur"] == 1){ ?>
+                   <li role="presentation" id="history-donnes-toggle"><a>Cours donnés</a></li>
+                   <li role="presentation" id="tarifs-toggle"><a>Tarifs</a></li>
+                   <li role="presentation" id="stats-toggle"><a>Statistiques</a></li>
+                   <?php } ?>
                </ul>
                <section id="infos">
                		<form method="post" role="form" enctype="multipart/form-data">
@@ -169,7 +206,8 @@ if(isset($_POST["edit"])){
 					  <input type="submit" name="edit" role="button" class="btn btn-primary btn-block" value="ENREGISTRER LES MODIFICATIONS">
 					  </form>
                </section>
-               <section id="history">
+               <?php if($details["est_eleve"] == 1){ ?>
+               <section id="history-suivis">
                	<table class="table table-striped">
                		<thead>
                			<tr>
@@ -181,7 +219,7 @@ if(isset($_POST["edit"])){
                			</tr>
                		</thead>
                		<tbody>
-               		<?php while($history = $queryHistory->fetch(PDO::FETCH_ASSOC)){ ?>
+               		<?php while($history = $queryHistoryRecus->fetch(PDO::FETCH_ASSOC)){ ?>
                			<tr <?php echo ($history["produit_adherent_id"]==null)?"class='warning'":"";?>>
                				<td class="col-lg-2"><?php echo $history['cours_intitule']." ".$history['cours_suffixe'];?></td>
                				<td class="col-lg-3"><?php echo date_create($history['cours_start'])->format('d/m/Y H:i');?> - <?php echo date_create($history['cours_end'])->format('H:i');?></td>
@@ -265,6 +303,89 @@ if(isset($_POST["edit"])){
                        </tbody>
                    </table>
                </section>
+               <?php } if($details["est_professeur"] == 1){ ?>
+               <section id="history-donnes">
+                 <div class="filter-options col-lg-6">
+                 	<p class="section-title">Options de filtrage</p>
+                 </div>
+                  <div class="price-summary col-lg-6">
+                       <p class="section-title">TOTAL</p>
+                       <p>Nombre de cours : <?php echo $queryHistoryDonnes->rowCount();?></p>
+                       <p>Somme totale : <?php echo $totalPrice;?> €</p>
+                       <p>Somme déjà réglée : <?php echo $totalPaid;?> €</p>
+                       <p>Somme restante : <?php echo $totalDue = $totalPrice - $totalPaid;?> €</p>
+                   </div>
+                   <div id="cours-list">
+                   	<table class="table table-striped">
+                   	    <thead>
+                   	        <tr>
+                   	            <th>Intitulé <span class="glyphicon glyphicon-sort sort" data-sort="cours-name"></span></th>
+                   	            <th>Jour <span class="glyphicon glyphicon-sort sort" data-sort="jour"></span></th>
+                   	            <th>Niveau <span class="glyphicon glyphicon-sort sort" data-sort="niveau"></span></th>
+                   	            <th>Lieu <span class="glyphicon glyphicon-sort sort" data-sort="lieu"></span></th>
+                   	            <th>Somme <span class="glyphicon glyphicon-sort sort" data-sort="montant"></span></th>
+                   	        </tr>
+                   	    </thead>
+                   	    <tbody class="list">
+                   	        <?php while ($history = $queryHistoryDonnes->fetch(PDO::FETCH_ASSOC)){?>
+                   	        <tr>
+                   	            <td class="cours-name"><?php echo $history['cours_intitule']." ".$history['cours_suffixe'];?></td>
+                   	            <td class="jour"><?php echo date_create($history['cours_start'])->format('d/m/Y H:i');?> - <?php echo date_create($history['cours_end'])->format('H:i');?></td>
+                   	            <td class="niveau"><?php echo $history['niveau_name'];?></td>
+                   	            <td class="lieu"><?php echo $history['salle_name'];?></td>
+                   	            <td class="<?php echo ($history['paiement_effectue'] != 0)?'payment-done':'payment-due';?> montant"><?php echo $history['cours_prix'];?> €</td>
+                   	        </tr>
+                   	        <?php $totalPrice += $history['cours_prix'];
+                   	if($history['paiement_effectue'] != 0)$totalPaid += $history['cours_prix'];} ?>
+                   	    </tbody>
+                   	</table>
+                   </div>
+               </section><!-- Historique des cours -->
+               <section id="tarifs">
+                   <table class="table table-striped">
+                       <thead>
+                           <tr>
+                               <th class="col-sm-3">Intitulé</th>
+                               <th class="col-sm-3">Prix</th>
+                               <th class="col-sm-3">Coefficient</th>
+                               <th class="col-sm-3"></th>
+                           </tr>
+                       </thead>
+                       <tbody id="table-content">
+							<tr id="new-tarif" style="display:none;">
+								<td class="col-sm-3">
+									<select name="prestation" id="prestation" class="form-control">
+									<?php while($prestations = $queryPrestations->fetch(PDO::FETCH_ASSOC)){ ?>
+										<option value="<?php echo $prestations["prestations_id"];?>"><?php echo $prestations["prestations_name"];?></option>
+									<?php } ?>
+									</select>
+								</td>
+								<td class="col-sm-3"><input type="text" name="tarif" id="tarif" class="form-control"></td>
+								<td class="col-sm-3">
+									<select name="ratio" id="ratio" class="form-control">
+									<?php
+									while ($row_ratio = $ratio->fetch(PDO::FETCH_ASSOC)){
+										$array_suffixes = preg_split("/','/", substr($row_ratio['Type'], 5, strlen($row_ratio['Type'])-7));
+										for($i = 0; $i < sizeof($array_suffixes); $i++){?>
+										<option value="<?php echo $array_suffixes[$i];?>"><?php echo $array_suffixes[$i];?></option>
+										<?php }
+									} ?>
+									</select>
+								</td>
+								<td class="col-sm-3"><button class="btn btn-default" onClick="addTarif()"><span class="glyphicon glyphicon-plus"></span> Valider</button><button class="btn btn-default" id="cancel"><span class="glyphicon glyphicon-cancel"></span> Annuler</button></td>
+							</tr>
+                     	<input type="hidden" name="prof_id" id="prof_id" value="<?php echo $data;?>">
+                       </tbody>
+                   </table>
+                   <button class="btn btn-primary" id="add-tarif">AJOUTER UN TARIF</button>
+                   <p id="json-output"></p>
+               </section> <!-- Tarifs -->
+               <section id="stats">
+               	<p>Nombre de cours</p>
+               	<div id="nombre-cours" style="height: 250px;"></div>
+               	<p>Types de cours donnés</p>
+               </section> <!-- Statistiques -->
+               <?php } ?>
            </div>
        </div>
    </div>
@@ -324,6 +445,117 @@ if(isset($_POST["edit"])){
 		   browseLabel: "Photo",
 		   browseIcon: '<i class="glyphicon glyphicon-picture"></i>'
 	   });
+       <?php if($details["est_professeur"] == 1){?>
+       
+       $("#add-tarif").click(function(){
+           $("#new-tarif").show();
+       });
+	   
+	   $("#cancel").click(function(){
+		   $("#new-tarif").hide();
+	   });
+	   
+	   $(document).ready(function(){
+		   fetchTarifs();
+		   
+		   var options = {
+			   valueNames: ['cours-name', 'jour', 'niveau', 'lieu', 'montant']
+		   };
+		   var coursList = new List('cours-list', options);
+		   
+		   var prof_id = <?php echo $data;?>;
+		   $.post('functions/compile_prof_cours.php', {prof_id}).done(function(data){
+			   var listeCours = JSON.parse(data);
+			   
+			   // Nombre de cours par jour
+			   var daysArray = [["lundi",0], ["mardi",0], ["mercredi",0], ["jeudi",0], ["vendredi",0], ["samedi",0]];
+			   var resDays = [];
+			   for (var j = 0; j < daysArray.length; j++){
+				   for (var i = 0; i < listeCours.length; i++){
+					   var date = moment(listeCours[i].day).locale('fr').format('dddd');
+					   if(daysArray[j][0] == date){
+						   daysArray[j][1]++;
+					   }
+				   }
+				   var graphBar = {};
+				   graphBar.d = daysArray[j][0];
+				   graphBar.a = daysArray[j][1];
+				   resDays.push(graphBar);
+			   }
+			   console.log(resDays);
+			   new Morris.Bar({
+				   element: 'nombre-cours',
+				   data : resDays,
+				   xkey: 'd',
+				   ykeys: ['a'],
+				   labels: ['Nombre de cours']
+			   });
+		   })
+		   
+	   });
+	   
+	   function addTarif(){
+		   var prof_id = $("#prof_id").val();
+		   var prestation = $("#prestation").val();
+		   var tarif = $("#tarif").val();
+		   var ratio = $("#ratio").val();
+		   $.post("functions/add_tarif_prof.php", {prof_id, prestation, tarif, ratio}).success(function(data){
+			   $("#new-tarif").hide();
+			   showSuccessNotif(data);
+			   $(".fetched").remove();
+			   fetchTarifs();
+		   })
+	   };
+	   
+	   function fetchTarifs(){
+		   var id = $("#prof_id").val();
+		   $.post("functions/get_tarifs.php", {id}).done(function(data){
+			   var json = JSON.parse(data);
+			   for(var i = 0; i < json.length; i++){
+				   var line = "<tr class='fetched' id='tarif-"+json[i].id+"'>";
+				   line += "<td class='col-sm-3 tarif-nom'>";
+				   line += json[i].prestation;
+				   line += "</td><td class='col-sm-3 tarif-prix'><span contenteditable='true' onblur='updateTarif("+json[i].id+")'>";
+				   line += json[i].tarif;
+				   line += "</span> € </td><td class='col-sm-3 tarif-ratio'>";
+				   line += json[i].ratio;
+				   line += "</td><td class='col-sm-3'>";
+				   line += "<button class='btn btn-default' onclick='deleteTarif("+json[i].id+")'><span class='glyphicon glyphicon-trash'></span> Supprimer</button>";
+				   line += "</td></tr>";
+				   $("#table-content").append(line);
+			   }
+		   });
+	   }
+	   
+	   function updateTarif(id){
+		   var update_id = id;
+		   var tarif = $("#tarif-"+update_id).children(".tarif-prix").children("span").html();
+		   $.post("functions/update_tarif_prof.php", {update_id, tarif}).done(function(data){
+			   showSuccessNotif(data);
+			   var originalColor = $("#tarif-"+update_id).css("background-color");
+			   var styles = {
+				   backgroundColor : "#dff0d8",
+				   transition: "0s"
+			   };
+			   var next = {
+				   backgroundColor : originalColor,
+				   transition : "2s"
+			   };
+			   $("#tarif-"+update_id).css(styles);
+			   setTimeout(function(){ $("#tarif-"+update_id).css(next); },800);
+		   });
+	   }
+	   
+	   function deleteTarif(id){
+		   var delete_id = id;
+		   alert(delete_id);
+		   $.post("functions/delete_tarif_prof.php", {delete_id}).done(function(data){
+			   showSuccessNotif(data);
+			   $(".fetched").remove();
+			   fetchTarifs();
+		   });
+	   }
+       <?php } ?>
 	</script>
 </body>
 </html>
