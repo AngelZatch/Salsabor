5b9c0c59cad72cd7e194f88fa23afda17cc12e5b
Andréas <pinbouen.andreas@gmail.com>
2015-08-26 19:47:12 +0200

    Insert date when a maturity is checked
    ADDED
    - When a maturity is recieved, the date of recepetion is automatically added
    to the entry in the database.

--- functions/validate_echeance.php
+++ functions/validate_echeance.php
@@ -3,10 +3,11 @@ require_once "db_connect.php";
 $db = PDOFactory::getConnection();
 
 $id = $_POST["echeance_id"];
+$date = date_create("now")->format("Y-m-d");
 
 try{
 	$db->beginTransaction();
-	$update = $db->prepare("UPDATE produits_echeances SET echeance_effectuee=1 WHERE id_echeance=?");
+	$update = $db->prepare("UPDATE produits_echeances SET echeance_effectuee=1, date_paiement=$date WHERE id_echeance=?");
 	$update->bindParam(1, $id);
 	$update->execute();
 	$db->commit();
