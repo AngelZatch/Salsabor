36fbc9b220ef78d7605cb5e86a501b82afe29c58
AngelZatch <pinbouen.andreas@gmail.com>
2016-03-18 11:40:54 +0100

    Fixed reactivation if hours remain
    FIXED
    - Putting products in active if hours remain only if dates allow so
    
    ADDED
    - Summoning the product modal will now compute the product and display accurate information

--- functions/compute_remaining_hours.php
+++ functions/compute_remaining_hours.php
@@ -15,7 +15,7 @@ Yes. This code does everything to ensure the information can be tracked and stay
 
 $product_id = $_POST["product_id"];
 
-$product_details = $db->query("SELECT volume_horaire, est_illimite, pa.date_activation AS produit_adherent_activation, validite_initiale, pa.actif AS produit_adherent_actif, date_achat,
+$product_details = $db->query("SELECT volume_horaire, est_illimite, pa.date_activation AS produit_adherent_activation, validite_initiale, pa.actif AS produit_adherent_actif, date_achat, date_expiration,
 						IF(date_prolongee IS NOT NULL, date_prolongee,
 							IF (date_fin_utilisation IS NOT NULL, date_fin_utilisation, date_expiration)
 							) AS produit_validity FROM produits_adherents pa
@@ -31,23 +31,25 @@ $sessions_list = $db->query("SELECT cours_unite, cours_start, cours_end FROM cou
 							ORDER BY cours_start ASC");
 
 $remaining_hours = $product_details["volume_horaire"];
-$date_fin_utilisation = $product_details["produit_validity"];
+if($product_details["est_illimite"] == 1){
+	$date_fin_utilisation = $product_details["date_expiration"];
+} else {
+	$date_fin_utilisation = $product_details["produit_validity"];
+}
 $values = array();
 $computeEnd = false;
 
 while($session = $sessions_list->fetch(PDO::FETCH_ASSOC)){
-	if($remaining_hours == $product_details["volume_horaire"] && $product_details["produit_adherent_actif"] != '1'){
-		if($session["cours_start"] >= $product_details["date_achat"]){
-			$date_activation = date_create($session["cours_start"])->format("Y-m-d 00:00:00");
-			$setActivationDate = $db->query("UPDATE produits_adherents SET date_activation = '$date_activation' WHERE id_produit_adherent = '$product_id'");
-			$computeEnd = true;
-		}
+	if($remaining_hours == $product_details["volume_horaire"] && $product_details["produit_adherent_actif"] != '1' && $session["cours_start"] >= $product_details["date_achat"]){
+		// If the product's current hours are max, the product is not an unlimited formula AND the date of the session is AFTER the purchase date of the product, we compute the activation date. This will only occur one time to ensure the date of activation is always accurate.
+		$date_activation = date_create($session["cours_start"])->format("Y-m-d 00:00:00");
+		$setActivationDate = $db->query("UPDATE produits_adherents SET date_activation = '$date_activation' WHERE id_produit_adherent = '$product_id'");
+		$computeEnd = true;
 	}
 	$remaining_hours -= floatval($session["cours_unite"]);
-	if($product_details["produit_validity"] == null || ($product_details["produit_validity"] < $session["cours_end"] && $remaining_hours >= 0)){
-		if($remaining_hours >= 0){
-			$date_fin_utilisation = $session["cours_end"];
-		}
+	if($product_details["produit_validity"] == null || ($product_details["produit_validity"] < $session["cours_end"] && $remaining_hours >= 0) || ($product_details["produit_validity"]) > $session["cours_end"] && $remaining_hours == 0){
+		// If there's no expiration date set for the product or (it is ANTERIOR to the date of the session BUT there's still hours on the product) or (it is POSTERIOR to the date of the session BUT there's no more hours on the product), the expiration date is set to the last session that happened.
+		$date_fin_utilisation = $session["cours_end"];
 	}
 }
 
@@ -80,25 +82,24 @@ if($remaining_hours <= 0){ // If the number of remaining hours is negative
 	echo 0;
 } else { // If the hours are still in positive.
 	array_push($values, $remaining_hours);
-	if($computeEnd){ // If the product was not active before and has to be activated.
+	if($computeEnd){ // If the expiration date has to be computed.
 		$date_fin_utilisation = date_create(computeExpirationDate($db, $date_activation, $product_details["validite_initiale"]))->format("Y-m-d H:i:s");
-		if($date_fin_utilisation < $product_details["produit_validity"]){
-			$status = '1';
-		} else {
-			$status = '2';
-		}
-		$update = $db->query("UPDATE produits_adherents
-						SET actif='$status', date_activation = '$date_activation', date_expiration='$date_fin_utilisation', volume_cours = '$remaining_hours'
-						WHERE id_produit_adherent = '$product_id'");
-	} else { // If the product was already active before.
-		if($product_details["produit_validity"] != '' && date_create($product_details["produit_validity"])->format("Y-m-d") < date("Y-m-d")){
+		if($date_fin_utilisation < date("Y-m-d")){ // If the computed expiration date is anterior to today, then the product should be expired.
 			$status = '2';
 		} else {
 			$status = '1';
 		}
+		// We remove the date of fully use, since hours remain.
 		$update = $db->query("UPDATE produits_adherents
-						SET actif='$status', volume_cours = '$remaining_hours'
+						SET actif='$status', date_activation = '$date_activation', date_expiration='$date_fin_utilisation', date_fin_utilisation = NULL, volume_cours = '$remaining_hours'
 						WHERE id_produit_adherent = '$product_id'");
+	} else { // It the expiration date doesn't have to be computed
+		if($product_details["produit_validity"] != '' && date_create($product_details["produit_validity"])->format("Y-m-d") < date("Y-m-d")){ // If there's an expiration date set AND it's anterior to today
+			$status = '2';
+		} else {
+			$status = '1';
+		}
+		$update = $db->query("UPDATE produits_adherents SET actif='$status', volume_cours = '$remaining_hours', date_fin_utilisation = NULL WHERE id_produit_adherent = '$product_id'");
 	}
 }
 array_push($values, $date_fin_utilisation); // Position 0 of the array
