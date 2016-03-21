c3c50d6f99fbe920f86637b3f9bc89315c2a2886
AngelZatch <pinbouen.andreas@gmail.com>
2016-03-21 14:01:42 +0100

    Display of user participations
    ADDED
    - Redone the page to display participations
    - New page displays irregular participations as well

--- user_history.php
+++ user_history.php
@@ -10,13 +10,24 @@ $queryDetails->execute();
 $details = $queryDetails->fetch(PDO::FETCH_ASSOC);
 
 // On obtient l'historique de ses cours
-$queryHistoryRecus = $db->prepare('SELECT * FROM cours_participants
+/*$queryHistoryRecus = $db->prepare('SELECT * FROM cours_participants
 							JOIN cours ON cours_id_foreign=cours.cours_id
 							JOIN niveau ON cours.cours_niveau=niveau.niveau_id
 							JOIN salle ON cours.cours_salle=salle.salle_id
 							JOIN produits_adherents ON produit_adherent_id=produits_adherents.id_produit_adherent
 							JOIN produits ON id_produit_foreign=produits.produit_id
-							WHERE eleve_id_foreign=?');
+							WHERE eleve_id_foreign=?');*/
+$queryHistoryRecus = $db->prepare('SELECT *, pa.date_activation AS produit_adherent_activation,
+							IF(date_prolongee IS NOT NULL, date_prolongee,
+								IF (date_fin_utilisation IS NOT NULL, date_fin_utilisation, date_expiration)
+								) AS produit_validity
+							FROM cours_participants cp
+							JOIN cours c ON cp.cours_id_foreign=c.cours_id
+							LEFT JOIN produits_adherents pa ON cp.produit_adherent_id=pa.id_produit_adherent
+							LEFT JOIN produits p ON pa.id_produit_foreign = p.produit_id
+							LEFT JOIN transactions t ON pa.id_transaction_foreign = t.id_transaction
+							WHERE eleve_id_foreign=?
+							ORDER BY cours_start DESC');
 $queryHistoryRecus->bindValue(1, $data);
 $queryHistoryRecus->execute();
 ?>
@@ -52,49 +63,32 @@ $queryHistoryRecus->execute();
 					</ul>
 					<div class="container-fluid participations-list-container">
 						<ul class="participations-list">
-							<?php while($history = $queryHistoryRecus->fetch(PDO::FETCH_ASSOC)){ ?>
-							<li class="participation container-fluid" data-argument="<?php echo $history["id"];?>" id="participation-<?php echo $history["id"];?>">
-								<p class="col-lg-12 session-title"><?php echo $history["cours_intitule"];?></p>
-								<p class="col-lg-12 session-hours"><?php echo $history["cours_start"];?></p>
+							<?php while($history = $queryHistoryRecus->fetch(PDO::FETCH_ASSOC)){
+	$date = date_create($history["cours_start"])->format("d/m/Y");
+	$hour_start = date_create($history["cours_start"])->format("H:i");
+	$hour_end = date_create($history["cours_end"])->format("H:i");?>
+							<li class="product-participation <?php echo ($history["produit_adherent_id"]==null)?"participation-over":"participation-valid";?> container-fluid" data-argument="<?php echo $history["id"];?>" id="participation-<?php echo $history["id"];?>">
+								<div class="col-lg-4">
+									<p class="col-lg-12 session-title"><?php echo $history["cours_intitule"];?></p>
+									<p class="col-lg-12 session-hours"><?php echo $date." : ".$hour_start." - ".$hour_end;?></p>
+								</div>
+								<div class="col-lg-8">
+									<?php if($history["produit_adherent_id"] == null){ ?>
+									<p class="col-lg-12 session-title">Pas de produit associé</p>
+									<?php } else {
+		$achat = date_create($history["date_achat"])->format("d/m/Y");
+		$activation_date = date_create($history["produit_adherent_activation"])->format("d/m/Y");
+		$expiration_date = date_create($history["produit_validity"])->format("d/m/Y");
+									?>
+									<p class="col-lg-12 session-title"><?php echo $history["produit_nom"];?></p>
+									<p class="col-lg-12 session-hours">Acheté le <?php echo $achat;?> / Valide du <?php echo $activation_date;?> au <?php echo $expiration_date;?> </p>
+									<?php } ?>
+								</div>
 							</li>
 
 							<?php } ?>
 						</ul>
 					</div>
-					<section id="history-suivis">
-						<table class="table table-striped">
-							<thead>
-								<tr>
-									<th class="col-lg-2">Intitulé</th>
-									<th class="col-lg-3">Jour</th>
-									<th class="col-lg-2">Détails</th>
-									<th class="col-lg-3">Forfait</th>
-									<th class="col-lg-2">Prix pondéré</th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php while($history = $queryHistoryRecus->fetch(PDO::FETCH_ASSOC)){ ?>
-								<tr <?php echo ($history["produit_adherent_id"]==null)?"class='warning'":"";?>>
-									<td class="col-lg-2"><?php echo $history['cours_intitule']." ".$history['cours_suffixe'];?></td>
-									<td class="col-lg-3"><?php echo date_create($history['cours_start'])->format('d/m/Y H:i');?> - <?php echo date_create($history['cours_end'])->format('H:i');?></td>
-									<td class="col-lg-2"><?php echo $history['niveau_name']."\n".$history['salle_name'];?></td>
-									<td class="col-lg-3">
-										<?php if($history["produit_adherent_id"]==null){?>
-										<a class="btn btn-info" name="link-forfait"><span class="glyphicon glyphicon-link"></span> Associer un forfait</a>
-										<input type="hidden" name="cours" value="<?php echo $history["cours_id"];?>">
-										<select name="forfaits-actifs" style="display:none;" class="form-control">
-											<?php while($forfaitsActifs = $queryForfaitsActifs->fetch(PDO::FETCH_ASSOC)){?>
-											<option value="<?php echo $forfaitsActifs["id_transaction"]?>"><?php echo $forfaitsActifs["produit_nom"];?></option>
-											<?php } ?>
-										</select>
-										<?php } else echo $history["produit_nom"];?>
-									</td>
-									<td class="col-lg-2">A déterminer</td>
-								</tr>
-								<?php } ?>
-							</tbody>
-						</table>
-					</section>
 				</div>
 			</div>
 		</div>
