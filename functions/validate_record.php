6f1b5c50226ebe5eba1e01eb53062675712d3828
AngelZatch <pinbouen.andreas@gmail.com>
2016-03-22 11:50:38 +0100

    Finding the correct product whatever happens
    ADDED
    - The application will now try to find the appropriate product for every type of product
    - Said product will be "Compute"'d once it's found separately.

--- functions/validate_record.php
+++ functions/validate_record.php
@@ -2,169 +2,82 @@
 require_once "db_connect.php";
 $db = PDOFactory::getConnection();
 
-$cours = $_POST["cours_id"];
-$eleve = $_POST["eleve_id"];
-$passage = $_POST["passage_id"];
-$rfid = $_POST["rfid"];
-$forfaitPrioriaire = $_POST["bundle_id"];
-$detailCours = $db->query("SELECT * FROM cours JOIN prestations ON cours_type=prestations.prestations_id WHERE cours_id=$cours")->fetch(PDO::FETCH_ASSOC);
-$prof = $db->query("SELECT * FROM tarifs_professeurs WHERE prof_id_foreign=$detailCours[prof_principal] AND type_prestation=$detailCours[cours_type]")->fetch(PDO::FETCH_ASSOC);
+$session_id = $_POST["session_id"];
+$user_id = $_POST["user_id"];
+$record_id = $_POST["record_id"];
+$product_id = $_POST["product_id"];
 
-$date_now = date_create("now")->format("Y-m-d 00:00:00");
+$cours_name = $db->query("SELECT cours_intitule FROM cours WHERE cours_id = '$session_id'")->fetch(PDO::FETCH_COLUMN);
 
-/** This code has to find the appropriate product to use for every single type of record ever **/
+/** This code has to find the appropriate product to use for every single type of record ever. Once it has found the appropriate record, it will return his number and then the JS will call "Compute" with it. **/
 
-try{
-	$db->beginTransaction();
-
-	if($forfaitPrioriaire == ""){
-		// Vérification de la présence d'une invitation
-		$queryInvitation = $db->query("SELECT *, produits_adherents.actif AS produitActif FROM produits_adherents JOIN produits ON id_produit_foreign=produits.produit_id WHERE id_user_foreign=$eleve AND produit_nom='Invitation' AND produits_adherents.actif='0'");
-		if($queryInvitation->rowCount() == '1'){
-			$produit = $queryInvitation->fetch(PDO::FETCH_ASSOC);
-			$actif = 0;
-
-			// Désactivation de l'invitation
-			$deactivate = $db->prepare("UPDATE produits_adherents SET date_fin_utilisation=?, actif=? WHERE id_produit_adherent=?");
-			$deactivate->bindParam(1, $date_now);
-			$deactivate->bindParam(2, $actif);
-			$deactivate->bindParam(3, $produit["id_produit_adherent"]);
-			$deactivate->execute();
-		} else {
-			$produit = $db->query("SELECT *, produits_adherents.actif AS produitActif
-									FROM produits_adherents
-									JOIN produits
-										ON id_produit_foreign=produits.produit_id
-									WHERE id_user_foreign=$eleve
-										AND produit_nom !='Invitation'
-										AND produit_nom != 'Adhésion Annuelle'
-										AND produits_adherents.actif != '2'
-									ORDER BY id_produit_adherent ASC")->fetch(PDO::FETCH_ASSOC);
-			// Vérification de la validité du forfait et activation si nécessaire
-			if($produit["produitActif"] == '0'){
-				$actif = 1;
-				$date_activation = $date_now;
-				$date_expiration = date("Y-m-d 00:00:00", strtotime($date_activation.'+'.$produit["validite_initiale"].'DAYS'));
-				$queryHoliday = $db->prepare("SELECT * FROM jours_chomes WHERE date_chomee >= ? AND date_chomee <= ?");
-				$queryHoliday->bindParam(1, $date_activation);
-				$queryHoliday->bindParam(2, $date_expiration);
-				$queryHoliday->execute();
-
-				$j = 0;
-
-				for($i = 0; $i <= $queryHoliday->rowCount(); $i++){
-					$exp_date = date("Y-m-d 00:00:00",strtotime($date_expiration.'+'.$i.'DAYS'));
-					$checkHoliday = $db->prepare("SELECT * FROM jours_chomes WHERE date_chomee=?");
-					$checkHoliday->bindParam(1, $exp_date);
-					$checkHoliday->execute();
-					if($checkHoliday->rowCount() != 0){
-						$j++;
+if($product_id == ""){ // If the product has not been manually set
+	// CASE WHERE THIS IS NOT A PRIVATE SESSION
+	if(preg_match("/jazz/i", $cours_name, $matches) || preg_match("/pilates/i", $cours_name, $matches) || preg_match("/particulier/i", $cours_name, $matches)){ // Search for specific Jazz, Pilates or private sessions
+		/*echo $matches[0];*/
+		$checkSpecific = $db->query("SELECT id_produit_adherent, id_produit_foreign, produit_nom, pa.actif AS produit_adherent_actif, date_achat FROM produits_adherents pa
+									JOIN produits p ON pa.id_produit_foreign = p.produit_id
+									JOIN transactions t ON pa.id_transaction_foreign = t.id_transaction
+									WHERE id_user_foreign='$user_id'
+									AND produit_nom LIKE '%$matches[0]%'
+									AND pa.actif != '2'
+									ORDER BY date_achat ASC");
+		if($checkSpecific->rowCount() > 0){
+			$product = $checkSpecific->fetch(PDO::FETCH_ASSOC);
+		} else { // First, we search for any freebies
+			$checkInvitation = $db->query("SELECT id_produit_adherent, id_produit_foreign, produit_nom, pa.actif AS produit_adherent_actif, date_achat FROM produits_adherents pa
+									JOIN produits p ON pa.id_produit_foreign = p.produit_id
+									JOIN transactions t ON pa.id_transaction_foreign = t.id_transaction
+									WHERE id_user_foreign='$user_id'
+									AND produit_nom = 'Invitation'
+									AND pa.actif = '0'
+									ORDER BY date_achat ASC");
+			if($checkInvitation->rowCount() > 0){ // If there are freebies still available, we take the first one.
+				$product = $checkInvitation->fetch(PDO::FETCH_ASSOC);
+			} else { // If no freebies, we look for every currently active products.
+				$checkActive = $db->query("SELECT id_produit_adherent, id_produit_foreign, produit_nom, pa.actif AS produit_adherent_actif, date_achat FROM produits_adherents pa
+									JOIN produits p ON pa.id_produit_foreign = p.produit_id
+									JOIN transactions t ON pa.id_transaction_foreign = t.id_transaction
+									WHERE id_user_foreign='$user_id'
+									AND produit_nom != 'Invitation'
+									AND pa.actif = '1'
+									AND est_abonnement = '0'
+									AND est_cours_particulier = '0'
+									ORDER BY date_achat ASC");
+				if($checkActive->rowCount() > 0){ // If there are active products that are not an annual sub
+					$product = $checkActive->fetch(PDO::FETCH_ASSOC);
+				} else { // We check inactive products now.
+					$checkPending = $db->query("SELECT id_produit_adherent, id_produit_foreign, produit_nom, pa.actif AS produit_adherent_actif, date_achat FROM produits_adherents pa
+									JOIN produits p ON pa.id_produit_foreign = p.produit_id
+									JOIN transactions t ON pa.id_transaction_foreign = t.id_transaction
+									WHERE id_user_foreign='$user_id'
+									AND produit_nom != 'Invitation'
+									AND pa.actif = '0'
+									AND est_abonnement = '0'
+									AND est_cours_particulier = '0'
+									ORDER BY date_achat ASC");
+					if($checkPending->rowCount() > 0){
+						$product = $checkPending->fetch(PDO::FETCH_ASSOC);
+					} else { // if there's NO product at all, we still accept but it goes into irregulars.
+						$product_id = NULL;
 					}
-					$totalOffset = $i + $j;
-					$new_exp_date = date("Y-m-d 00:00:00",strtotime($date_expiration.'+'.$totalOffset.'DAYS'));
-				}
-
-				$activate = $db->prepare("UPDATE produits_adherents SET date_activation=?, date_expiration=?, actif=? WHERE id_produit_adherent=?");
-				$activate->bindParam(1, $date_activation);
-				$activate->bindParam(2, $new_exp_date);
-				$activate->bindParam(3, $actif);
-				$activate->bindParam(4, $produit["id_produit_adherent"]);
-				$activate->execute();
-			}
-
-			// Déduction du volume horaire dans le forfait
-			if(isset($produit["id_produit_adherent"])){
-				if(!strstr($produit["produit_nom"], "Illimité")){
-					$substract = $db->prepare("UPDATE produits_adherents SET volume_cours=? WHERE id_produit_adherent=?");
-					$remainingHours = $produit["volume_cours"] - $detailCours["cours_unite"];
-					$substract->bindParam(1, $remainingHours);
-					$substract->bindParam(2, $produit["id_produit_adherent"]);
-					$substract->execute();
 				}
 			}
-
-			// Mise à jour de la rémunération du professeur
-			if($prof["ratio_multiplicatif"] == "personne"){
-				$prix = $detailCours["cours_prix"] + $prof["tarif_prestation"];
-				$add = $db->prepare("UPDATE cours SET cours_prix=? WHERE cours_id=?");
-				$add->bindParam(1, $prix);
-				$add->bindParam(2, $cours);
-				$add->execute();
-			}
-		}
-	} else {
-		$produit = $db->query("SELECT *, produits_adherents.actif AS produitActif FROM produits_adherents JOIN produits ON id_produit_foreign=produits.produit_id WHERE id_produit_adherent=$forfaitPrioriaire")->fetch(PDO::FETCH_ASSOC);
-		// Vérification de la validité du forfait et activation si nécessaire
-		if($produit["produitActif"] == '0'){
-			$actif = 1;
-			$date_activation = $date_now;
-			$date_expiration = date("Y-m-d 00:00:00", strtotime($date_activation.'+'.$produit["validite_initiale"].'DAYS'));
-			$queryHoliday = $db->prepare("SELECT * FROM jours_chomes WHERE date_chomee >= ? AND date_chomee <= ?");
-			$queryHoliday->bindParam(1, $date_activation);
-			$queryHoliday->bindParam(2, $date_expiration);
-			$queryHoliday->execute();
-
-			$j = 0;
-
-			for($i = 0; $i <= $queryHoliday->rowCount(); $i++){
-				$exp_date = date("Y-m-d 00:00:00",strtotime($date_expiration.'+'.$i.'DAYS'));
-				$checkHoliday = $db->prepare("SELECT * FROM jours_chomes WHERE date_chomee=?");
-				$checkHoliday->bindParam(1, $exp_date);
-				$checkHoliday->execute();
-				if($checkHoliday->rowCount() != 0){
-					$j++;
-				}
-				$totalOffset = $i + $j;
-				$new_exp_date = date("Y-m-d 00:00:00",strtotime($date_expiration.'+'.$totalOffset.'DAYS'));
-			}
-
-			$activate = $db->prepare("UPDATE produits_adherents SET date_activation=?, date_expiration=?, actif=? WHERE id_produit_adherent=?");
-			$activate->bindParam(1, $date_activation);
-			$activate->bindParam(2, $new_exp_date);
-			$activate->bindParam(3, $actif);
-			$activate->bindParam(4, $produit["id_produit_adherent"]);
-			$activate->execute();
-		}
-
-		// Déduction du volume horaire dans le forfait
-		if(isset($produit["id_produit_adherent"])){
-			if(!strstr($produit["produit_nom"], "Illimité")){
-				$substract = $db->prepare("UPDATE produits_adherents SET volume_cours=? WHERE id_produit_adherent=?");
-				$remainingHours = $produit["volume_cours"] - $detailCours["cours_unite"];
-				$substract->bindParam(1, $remainingHours);
-				$substract->bindParam(2, $produit["id_produit_adherent"]);
-				$substract->execute();
-			}
-		}
-
-		// Mise à jour de la rémunération du professeur
-		if($prof["ratio_multiplicatif"] == "personne"){
-			$prix = $detailCours["cours_prix"] + $prof["tarif_prestation"];
-			$add = $db->prepare("UPDATE cours SET cours_prix=? WHERE cours_id=?");
-			$add->bindParam(1, $prix);
-			$add->bindParam(2, $cours);
-			$add->execute();
 		}
 	}
-
-	// Enregistrement du passage dans la table des participants
-	$new = $db->prepare('INSERT INTO cours_participants(cours_id_foreign, eleve_id_foreign, produit_adherent_id)
-	VALUES(:cours, :eleve, :produit)');
-	$new->bindParam(':cours', $cours);
-	$new->bindParam(':eleve', $eleve);
-	$new->bindParam(':produit', $produit["id_produit_adherent"]);
-	$new->execute();
-
-	// Validation de l'enregistrement dans la table passage (indiquera que le passage a déjà été traité)
-	$update = $db->prepare("UPDATE passages SET cours_id=?, status=2 WHERE passage_id=?");
-	$update->bindParam(1, $cours);
-	$update->bindParam(2, $passage);
-	$update->execute();
-
-	$db->commit();
-	echo "Passage enregistré.";
-} catch(PDOException $e){
-	$db->rollBack();
-	echo $e->getMessage();
 }
+$product_id = $product["id_produit_adherent"];
+
+// Confirming the record as a fleshed out participation
+$new = $db->prepare("INSERT INTO cours_participants(cours_id_foreign, eleve_id_foreign, produit_adherent_id)
+						VALUES(:cours, :eleve, :produit)");
+$new->bindParam(':cours', $session_id);
+$new->bindParam(':eleve', $user_id);
+$new->bindParam(':produit', $product_id);
+$new->execute();
+
+// Update the record as handled with the correct session and status
+$update = $db->query("UPDATE passages SET cours_id='$session_id', status=2 WHERE passage_id='$record_id'");
+
+echo $product_id;
 ?>
--- tests.php
+++ tests.php
@@ -1,9 +1,6 @@
 <?php
 require_once 'functions/db_connect.php';
 $db = PDOFactory::getConnection();
-$produit = $db->query("SELECT * FROM produits_adherents
-						JOIN produits ON id_produit_foreign=produits.produit_id
-						WHERE id_produit_adherent=1")->fetch(PDO::FETCH_ASSOC);
 ?>
 <html>
 	<head>
@@ -22,10 +19,68 @@ $produit = $db->query("SELECT * FROM produits_adherents
 					</div>
 					<div class="col-lg-6"></div>
 				</div>
-				<div class="col-sm-10 main"><?php
-$today = date_create('now')->format('Y-m-d H:i:s');
-// Si ce même code a été passé il y a moins de 20 minutes, le passage est refusé
-echo $tenPrevious = date('Y-m-d H:i:s', strtotime($today.'-10MINUTES'));
+				<div class="col-sm-10 main">
+					<?php
+					$eleve = 10507;
+					$product_id = "";
+					$cours_id = 2819;
+					$cours_name = $db->query("SELECT cours_intitule FROM cours WHERE cours_id = '$cours_id'")->fetch(PDO::FETCH_COLUMN);
+					/*echo $cours_name;*/
+					if($product_id == ""){ // If the product has not been manually set
+						// CASE WHERE THIS IS NOT A PRIVATE SESSION
+						if(preg_match("/jazz/i", $cours_name, $matches) || preg_match("/pilates/i", $cours_name, $matches) || preg_match("/particulier/i", $cours_name, $matches)){ // Search for specific Jazz, Pilates or private sessions
+							/*echo $matches[0];*/
+							$checkSpecific = $db->query("SELECT id_produit_adherent, id_produit_foreign, produit_nom, pa.actif AS produit_adherent_actif, date_achat FROM produits_adherents pa
+									JOIN produits p ON pa.id_produit_foreign = p.produit_id
+									JOIN transactions t ON pa.id_transaction_foreign = t.id_transaction
+									WHERE id_user_foreign='$eleve'
+									AND produit_nom LIKE '%$matches[0]%'
+									AND pa.actif != '2'
+									ORDER BY date_achat ASC");
+							if($checkSpecific->rowCount() > 0){
+								$product = $checkSpecific->fetch(PDO::FETCH_ASSOC);
+							} else { // First, we search for any freebies
+								$checkInvitation = $db->query("SELECT id_produit_adherent, id_produit_foreign, produit_nom, pa.actif AS produit_adherent_actif, date_achat FROM produits_adherents pa
+									JOIN produits p ON pa.id_produit_foreign = p.produit_id
+									JOIN transactions t ON pa.id_transaction_foreign = t.id_transaction
+									WHERE id_user_foreign='$eleve'
+									AND produit_nom = 'Invitation'
+									AND pa.actif = '0'
+									ORDER BY date_achat ASC");
+								if($checkInvitation->rowCount() > 0){ // If there are freebies still available, we take the first one.
+									$product = $checkInvitation->fetch(PDO::FETCH_ASSOC);
+								} else { // If no freebies, we look for every currently active products.
+									$checkActive = $db->query("SELECT id_produit_adherent, id_produit_foreign, produit_nom, pa.actif AS produit_adherent_actif, date_achat FROM produits_adherents pa
+									JOIN produits p ON pa.id_produit_foreign = p.produit_id
+									JOIN transactions t ON pa.id_transaction_foreign = t.id_transaction
+									WHERE id_user_foreign='$eleve'
+									AND produit_nom != 'Invitation'
+									AND pa.actif = '1'
+									AND est_abonnement = '0'
+									AND est_cours_particulier = '0'
+									ORDER BY date_achat ASC");
+									if($checkActive->rowCount() > 0){ // If there are active products that are not an annual sub
+										$product = $checkActive->fetch(PDO::FETCH_ASSOC);
+									} else { // We check inactive products now.
+										$checkPending = $db->query("SELECT id_produit_adherent, id_produit_foreign, produit_nom, pa.actif AS produit_adherent_actif, date_achat FROM produits_adherents pa
+									JOIN produits p ON pa.id_produit_foreign = p.produit_id
+									JOIN transactions t ON pa.id_transaction_foreign = t.id_transaction
+									WHERE id_user_foreign='$eleve'
+									AND produit_nom != 'Invitation'
+									AND pa.actif = '0'
+									AND est_abonnement = '0'
+									AND est_cours_particulier = '0'
+									ORDER BY date_achat ASC");
+										if($checkPending->rowCount() > 0){
+											$product = $checkPending->fetch(PDO::FETCH_ASSOC);
+										}
+									}
+								}
+							}
+						}
+					}
+					echo $product_id = $product["id_produit_adherent"];
+					echo "<br>";
 					?>
 				</div>
 			</div>
