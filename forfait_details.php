ecd8eac7a2d9da417390c8db5e063b1e4d2bf1e7
AngelZatch <pinbouen.andreas@gmail.com>
2016-04-01 11:37:47 +0200

    Validity in days or weeks
    ADDED
    - Can now set validity in days.

--- forfait_details.php
+++ forfait_details.php
@@ -16,7 +16,11 @@ if(isset($_POST["edit"])){
 	} else {
 		$tarif_horaire = 0;
 	}
-	$validite = 7 * $_POST["validite"];
+	if($_POST["validite_jour"] == "1"){
+		$validite = $_POST["validite"];
+	} else {
+		$validite = 7 * $_POST["validite"];
+	}
 	$actif = 1;
 	if(isset($_POST["arep"])){
 		$arep = $_POST["arep"];
@@ -77,6 +81,7 @@ if(isset($_POST["edit"])){
 	<head>
 		<meta charset="UTF-8">
 		<title>Détails du forfait <?php echo $produit["produit_nom"];?> | Salsabor</title>
+		<base href="../">
 		<?php include "styles.php";?>
 	</head>
 	<body>
@@ -84,84 +89,90 @@ if(isset($_POST["edit"])){
 		<div class="container-fluid">
 			<div class="row">
 				<?php include "side-menu.php";?>
-				<form action="forfait_details.php?id=<?php echo $data;?>" method="post">
-					<div class="col-lg-10 col-lg-offset-2 main">
+				<div class="col-lg-10 col-lg-offset-2 main">
+					<form action="" class="form-horizontal" method="post">
 						<legend><span class="glyphicon glyphicon-credit-card"></span> Forfait <?php echo $produit["produit_nom"];?>
 							<input type="submit" name="edit" role="button" class="btn btn-primary" value="ENREGISTRER LES MODIFICATIONS">
 						</legend>
 						<div class="form-group">
-							<label for="intitule">Intitulé</label>
-							<input type="text" class="form-control input-lg" name="intitule" value="<?php echo $produit["produit_nom"];?>" placeholder="Nom du produit">
+							<label for="intitule" class="control-label col-lg-3">Intitulé</label>
+							<div class="col-lg-9">
+								<input type="text" class="form-control" name="intitule" value="<?php echo $produit["produit_nom"];?>" placeholder="Nom du produit">
+							</div>
 						</div>
 						<div class="form-group">
-							<label for="flags">Type de produit</label><br>
-							<label for="est_recharge" class="control-label">Recharge Liberté</label>
-							<input name="est_recharge" id="est_recharge" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_recharge"];?>">
-							<label for="est_illimite" class="control-label">Offre Illimitée</label>
-							<input name="est_illimite" id="est_illimite" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_illimite"];?>">
-							<label for="est_sans_engagement" class="control-label">Sans Engagement</label>
-							<input name="est_sans_engagement" id="est_sans_engagement" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_sans_engagement"];?>">
-							<label for="est_cours_particulier" class="control-label">Cours Particulier</label>
-							<input name="est_cours_particulier" id="est_cours_particulier" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_cours_particulier"];?>">
-							<label for="est_formation_professionnelle" class="contorl-label">Formation Professionnelle</label>
-							<input name="est_formation_professionnelle" id="est_formation_professionnelle" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_formation_professionnelle"];?>">
-							<label for="est_abonnement" class="contorl-label">Abonnement</label>
-							<input name="est_abonnement" id="est_abonnement" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_abonnement"];?>">
-							<label for="est_autre" class="contorl-label">Divers</label>
-							<input name="est_autre" id="est_autre" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_autre"];?>">
+							<label for="flags" class="control-label col-lg-3">Type de produit</label><br>
+							<div class="col-lg-9">
+								<label for="est_recharge" class="control-label">Recharge Liberté</label>
+								<input name="est_recharge" id="est_recharge" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_recharge"];?>">
+								<label for="est_illimite" class="control-label">Illimité</label>
+								<input name="est_illimite" id="est_illimite" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_illimite"];?>">
+								<label for="est_sans_engagement" class="control-label">Sans Engagement</label>
+								<input name="est_sans_engagement" id="est_sans_engagement" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_sans_engagement"];?>">
+								<label for="est_cours_particulier" class="control-label">Cours Particulier</label>
+								<input name="est_cours_particulier" id="est_cours_particulier" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_cours_particulier"];?>">
+								<label for="est_formation_professionnelle" class="contorl-label">Formation Pro.</label>
+								<input name="est_formation_professionnelle" id="est_formation_professionnelle" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_formation_professionnelle"];?>">
+								<label for="est_abonnement" class="contorl-label">Abonnement</label>
+								<input name="est_abonnement" id="est_abonnement" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_abonnement"];?>">
+								<label for="est_autre" class="contorl-label">Divers</label>
+								<input name="est_autre" id="est_autre" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="<?php echo $produit["est_autre"];?>">
+							</div>
 						</div>
 						<div class="form-group">
-							<label for="description">Description</label>
-							<textarea rows="5" class="form-control input-lg" name="description" value="<?php echo $produit["description"];?>" placeholder=""></textarea>
+							<label for="description" class="col-lg-3 control-label">Description</label>
+							<div class="col-lg-9">
+								<textarea rows="5" class="form-control" name="description" value="<?php echo $produit["description"];?>" placeholder=""></textarea>
+							</div>
 						</div>
-						<div class="row">
-							<div class="col-lg-3">
-								<div class="form-group">
-									<label for="volume_horaire">Volume de cours (en heures)</label>
-									<input type="number" class="form-control input-lg" name="volume_horaire" value="<?php echo $produit["volume_horaire"];?>" placeholder="Exemple : 10">
-								</div>
+						<div class="form-group">
+							<label for="volume_horaire" class="col-lg-3 control-label">Volume de cours (en heures)</label>
+							<div class="col-lg-9">
+								<input type="number" class="form-control" name="volume_horaire" value="<?php echo $produit["volume_horaire"];?>" placeholder="Exemple : 10">
 							</div>
-							<div class="col-lg-3">
-								<div class="form-group">
-									<label for="validite">Durée de validité (en semaines)</label>
-									<input type="number" class="form-control input-lg" name="validite" value="<?php echo $produit["validite_initiale"] / 7;?>" placeholder="Exemple : 48">
-								</div>
+						</div>
+						<div class="form-group">
+							<label for="validite" class="col-lg-3 control-label">Durée de validité</label>
+							<div class="col-lg-9">
+								<input type="number" class="form-control" name="validite" value="<?php echo $produit["validite_initiale"];?>" placeholder="Exemple : 48">
+								<label for="est_recharge" class="control-label">Jours</label>
+								<input name="validite_jour" id="validite_jour" data-toggle="checkbox-x" data-size="lg" data-three-state="false" value="1"><span class="label-tip">Si décoché, la durée sera calculée en semaines.</span>
 							</div>
-							<div class="col-lg-3">
-								<div class="form-group">
-									<label for="tarif_global">Prix d'achat</label>
-									<div class="input-group input-group-lg">
-										<input type="number" step="any" class="form-control" name="tarif_global" value="<?php echo $produit["tarif_global"];?>">
-										<span class="input-group-addon">€</span>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-3">
-								<div class="form-group">
-									<label for="echeances">Nombre d'échéances autorisées</label>
-									<input type="number" class="form-control input-lg" name="echeances" value="<?php echo $produit["echeances_paiement"];?>">
+						</div>
+						<div class="form-group">
+							<label for="tarif_global" class="col-lg-3 control-label">Prix d'achat</label>
+							<div class="col-lg-9">
+								<div class="input-group">
+									<input type="number" step="any" class="form-control" name="tarif_global" value="<?php echo $produit["tarif_global"];?>">
+									<span class="input-group-addon">€</span>
 								</div>
 							</div>
 						</div>
-						<div class="row">
-							<div class="col-lg-6">
-								<div class="form-group">
-									<label for="date_activation">Ouverture à l'achat <span class="label-tip">Dans le cas d'une offre promotionnelle limitée dans le temps</span></label>
-									<div class="input-group input-group-lg">
-										<input type="date" class="form-control" name="date_activation" value="<?php echo $produit["date_activation"];?>">
-										<span role="button" class="input-group-btn">
-											<a class="btn btn-info" role="button" date-today="true">Insérer aujourd'hui</a>
-										</span>
-									</div>
+						<div class="form-group">
+							<label for="echeances" class="col-lg-3 control-label">Nombre d'échéances autorisées</label>
+							<div class="col-lg-9">
+								<input type="number" class="form-control" name="echeances" value="<?php echo $produit["echeances_paiement"];?>">
+							</div>
+						</div>
+						<p class="form-section">Période de vente</p>
+						<span class="label-tip">Dans le cas d'une offre promotionnelle limitée dans le temps</span>
+						<div class="form-group">
+							<label for="date_activation" class="col-lg-3 control-label">Ouverture à l'achat</label>
+							<div class="col-lg-9">
+								<div class="input-group">
+									<input type="date" class="form-control" name="date_activation" value="<?php echo $produit["date_activation"];?>">
+									<span role="button" class="input-group-btn">
+										<a class="btn btn-info" role="button" date-today="true">Insérer aujourd'hui</a>
+									</span>
 								</div>
 							</div>
-							<div class="col-lg-6">
-								<div class="form-group">
-									<label for="date_limite_achat">Fermeture à l'achat  <span class="label-tip">Dans le cas d'une offre promotionnelle limitée dans le temps</span></label>
-									<div class="input-group input-group-lg">
-										<input type="date" class="form-control" name="date_limite_achat" value="<?php echo $produit["date_desactivation"];?>">
-										<span role="buttton" class="input-group-btn"><a class="btn btn-info" role="button" date-today="true">Insérer aujourd'hui</a></span>
-									</div>
+						</div>
+						<div class="form-group">
+							<label for="date_limite_achat" class="col-lg-3 control-label">Fermeture à l'achat</label>
+							<div class="col-lg-9">
+								<div class="input-group">
+									<input type="date" class="form-control" name="date_limite_achat" value="<?php echo $produit["date_desactivation"];?>">
+									<span role="buttton" class="input-group-btn"><a class="btn btn-info" role="button" date-today="true">Insérer aujourd'hui</a></span>
 								</div>
 							</div>
 						</div>
@@ -169,8 +180,8 @@ if(isset($_POST["edit"])){
 							<label for="arep">Autoriser l'extension de validité ?</label>
 							<input type="checkbox" value="1" name="arep">
 						</div>
-					</div>
-				</form>
+					</form>
+				</div>
 			</div>
 		</div>
 		<?php include "scripts.php";?>
