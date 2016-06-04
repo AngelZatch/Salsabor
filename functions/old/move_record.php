3f0ea9c90be2628a4abf366133f199a5b9f1c936
AngelZatch <pinbouen.andreas@gmail.com>
2016-06-04 11:10:12 +0200

    Cleaning unused files
    DELETED
    - Obsolete code

--- functions/old/move_record.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "db_connect.php";
-$db = PDOFactory::getConnection();
-
-$passage_id = $_POST["passage_id"];
-$cours_id = $_POST["target_id"];
-$db->query("UPDATE passages SET cours_id=$cours_id, status=2 WHERE passage_id=$passage_id");
-echo "Passage déplacé et validé";
-?>
