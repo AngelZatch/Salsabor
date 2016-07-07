51e494758c4b512f868299ffe54e1e1fd173e262
AngelZatch <pinbouen.andreas@gmail.com>
2016-07-07 13:09:59 +0200

    Beta implementation of the new session-product match
    ADDED
    - Beta ver. of script that will match sessions to products based on tags

--- external/lire_rfid.php
+++ external/lire_rfid.php
@@ -7,16 +7,17 @@ $data = explode('*', $_GET["carte"]);
 $tag_rfid = $data[0];
 $reader_token = $data[1];
 
-prepareParticipation($db, $tag_rfid, $reader_token);
+//prepareParticipation($db, $tag_rfid, $reader_token);
+prepareParticipationBeta($db, $tag_rfid, $reader_token);
 
-function prepareParticipation($db, $tag, $reader_token){
+function prepareParticipation($db, $user_tag, $reader_token){
 	$today = date("Y-m-d H:i:s");
 	//$limit = date("Y-m-d H:i:s", strtotime($today.'+20MINUTES'));
 	if($reader_token == "192.168.0.3"){
 		$status = "1";
 		$new = $db->query("INSERT INTO participations(user_rfid, room_token, passage_date, status)
-					VALUES('$tag', '$reader_token', '$today', '$status')");
-		echo $ligne = $today.";".$tag.";".$reader_token."$";
+					VALUES('$user_tag', '$reader_token', '$today', '$status')");
+		echo $ligne = $today.";".$user_tag.";".$reader_token."$";
 	} else {
 		// If the tag is not for associating, we search a product that could be used for this session.
 		// First, we get the name of the session and the ID of the user.
@@ -27,7 +28,7 @@ function prepareParticipation($db, $tag, $reader_token){
 								WHERE ouvert = '1' AND reader_token = '$reader_token'")->fetch(PDO::FETCH_GROUP);
 		$cours_name = $session["cours_intitule"];
 		$session_id = $session["cours_id"];
-		$user_details = $db->query("SELECT user_id, mail FROM users WHERE user_rfid = '$tag'")->fetch(PDO::FETCH_ASSOC);
+		$user_details = $db->query("SELECT user_id, mail FROM users WHERE user_rfid = '$user_tag'")->fetch(PDO::FETCH_ASSOC);
 
 		if(preg_match("/@/", $user_details["mail"], $matches)){
 			$notification = $db->query("INSERT IGNORE INTO team_notifications(notification_token, notification_target, notification_date, notification_state)
@@ -35,13 +36,40 @@ function prepareParticipation($db, $tag, $reader_token){
 		}
 
 		// Ok, we got everything, let's look for potential duplicates
-		$duplicates = $db->query("SELECT * FROM participations WHERE user_rfid = '$tag' AND cours_id = '$session_id'")->rowCount();
+		$duplicates = $db->query("SELECT * FROM participations WHERE user_rfid = '$user_tag' AND cours_id = '$session_id'")->rowCount();
 
 		if($duplicates > 0){
-			echo $ligne = $today.";".$tag.";".$reader_token."$-3";
+			echo $ligne = $today.";".$user_tag.";".$reader_token."$-3";
 		} else {
-			addParticipation($db, $cours_name, $session_id, $user_details["user_id"], $reader_token, $tag);
+			addParticipation($db, $cours_name, $session_id, $user_details["user_id"], $reader_token, $user_tag);
 		}
+	}
+}
+
+function prepareParticipationBeta($db, $user_tag, $reader_token){
+	$today = date("Y-m-d H:i:s");
+	if($reader_token == "192.168.0.3"){
+		$status = "1";
+		$new = $db->query("INSERT INTO participations(user_rfid, room_token, passage_date, status)
+					VALUES('$user_tag', '$reader_token', '$today', '$status')");
+		echo "$";
+	} else {
+		// If the tag is not for associating, we search a product that could be used for this session.
+		// First, we get the name of the session and the ID of the user.
+		// For the session, we have to find it based on the time of the record and the position.
+		$session = $db->query("SELECT cours_id FROM cours c
+								JOIN rooms r ON c.cours_salle = r.room_id
+								WHERE ouvert = '1' AND room_reader = '$reader_token'")->fetch(PDO::FETCH_COLUMN);
+		$session_id = $session["cours_id"];
+		$user_details = $db->query("SELECT user_id, mail FROM users WHERE user_rfid = '$user_tag'")->fetch(PDO::FETCH_ASSOC);
+
+		if(!preg_match("/@/", $user_details["mail"], $matches)){
+			$notification = $db->query("INSERT IGNORE INTO team_notifications(notification_token, notification_target, notification_date, notification_state)
+								VALUES('MAI', '$user_details[user_id]', '$today', '1')");
+		}
+
+		addParticipationBeta($db, $today, $session_id, $user_details["user_id"], $reader_token, $user_tag);
+		/*addParticipation($db, $cours_name, $session_id, $user_details["user_id"], $reader_token, $tag);*/
 	}
 }
 // The reader expects this:
--- read_rfid.php
+++ read_rfid.php
@@ -17,29 +17,32 @@ if(isset($_POST["add"])){
 	prepareParticipation($db, $tag_rfid, $reader_token);
 }
 
-function prepareParticipation($db, $tag, $reader_token){
+function prepareParticipation($db, $user_tag, $reader_token){
 	$today = date("Y-m-d H:i:s");
 	if($reader_token == "192.168.0.3"){
 		$status = "1";
+		$new = $db->query("INSERT INTO participations(user_rfid, room_token, passage_date, status)
+					VALUES('$user_tag', '$reader_token', '$today', '$status')");
+		echo "$";
 	} else {
 		// If the tag is not for associating, we search a product that could be used for this session.
 		// First, we get the name of the session and the ID of the user.
 		// For the session, we have to find it based on the time of the record and the position.
-		$session = $db->query("SELECT cours_intitule, cours_id FROM cours c
+		$session = $db->query("SELECT cours_id FROM cours c
 								JOIN rooms r ON c.cours_salle = r.room_id
-								WHERE ouvert = '1' AND room_reader = '$reader_token'")->fetch(PDO::FETCH_GROUP);
-		$cours_name = $session["cours_intitule"];
+								WHERE ouvert = '1' AND room_reader = '$reader_token'")->fetch(PDO::FETCH_COLUMN);
 		$session_id = $session["cours_id"];
-		$user_details = $db->query("SELECT user_id, mail FROM users WHERE user_rfid = '$tag'")->fetch(PDO::FETCH_ASSOC);
+		$user_details = $db->query("SELECT user_id, mail FROM users WHERE user_rfid = '$user_tag'")->fetch(PDO::FETCH_ASSOC);
 
 		if(!preg_match("/@/", $user_details["mail"], $matches)){
 			$notification = $db->query("INSERT IGNORE INTO team_notifications(notification_token, notification_target, notification_date, notification_state)
 								VALUES('MAI', '$user_details[user_id]', '$today', '1')");
 		}
 
-		addParticipation($db, $cours_name, $session_id, $user_details["user_id"], $reader_token, $tag);
+		addParticipationBeta($db, $today, $session_id, $user_details["user_id"], $reader_token, $user_tag);
+		/*addParticipation($db, $cours_name, $session_id, $user_details["user_id"], $reader_token, $tag);*/
 	}
-	header('Location: passages');
+	/*header('Location: passages');*/
 }
 ?>
 <html>
