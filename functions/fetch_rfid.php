bb04c02bbc4afaff2fb72c757f9d36bb3765492a
AngelZatch <pinbouen.andreas@gmail.com>
2016-11-24 09:27:48 +0100

    RFID fetch
    MODIFIED
    - The RFID fetch will now search from the moment the button is clicked, dismissing every previous record.

--- functions/fetch_rfid.php
+++ functions/fetch_rfid.php
@@ -1,10 +1,14 @@
 <?php
 require_once "../functions/db_connect.php";
 $db = PDOFactory::getConnection();
+
+$time = date("Y-m-d H:i:s");
 $status = "1";
-$rfid = $db->prepare('SELECT user_rfid FROM participations WHERE status=?');
+
+$rfid = $db->prepare('SELECT user_rfid FROM participations WHERE status=? AND passage_date > ?');
 $rfid->bindParam(1, $status);
+$rfid->bindParam(2, $time);
 $rfid->execute();
-$res = $rfid->fetch(PDO::FETCH_ASSOC);
-echo $res["user_rfid"];
+
+echo $rfid->fetch(PDO::FETCH_COLUMN);
 ?>
